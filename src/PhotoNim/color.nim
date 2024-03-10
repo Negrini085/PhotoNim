10a57a34c981eb0c9183bc1badc5da7e392b3ac4
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-03-10 18:55:40 +0100

    feat(multByScalar): allows multiplication of a Color variable by a float number

--- src/PhotoNim/color.nim
+++ src/PhotoNim/color.nim
@@ -7,6 +7,18 @@ type
 
 proc newColor*(r, g, b: float32): Color {.inline.} = Color(r: r, g: g, b: b)
 
+
+
+proc multByScalar*(color: Color, scalar: float32) : Color = 
+    ## Multiplication by a scalar
+    result.r = color.r * scalar
+    result.g = color.g * scalar
+    result.b = color.b * scalar
+
+    return result
+
+
+
 proc areClose*(x, y: Color): bool {.inline.} = 
     ## Check if two Color are the same up to numerical precision using areClose
     areClose(x.r, y.r) and areClose(x.g, y.g) and areClose(x.b, y.b)
