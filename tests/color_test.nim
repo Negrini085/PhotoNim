c0d4d93654829e3bf5ce113d2914010a195e374c
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-03-10 19:45:08 +0100

    test(multByScal): checks multiplication by scalar

--- tests/color_test.nim
+++ tests/color_test.nim
@@ -5,10 +5,18 @@ import PhotoNim/color
 suite "ColorTest":
     setup:
         let col1 = newColor(1.0, 2.0, 3.0)
-        let col2 = Color()
+        var col2 = Color()
 
     test "newColor":
+        ## Constructor test
         check areClose(col1.r, 1.0) 
         check areClose(col1.g, 2.0) 
         check areClose(col1.b, 3.0)
         check areClose(col2, newColor(0.0, 0.0, 0.0))
+
+    test "multByScal":
+        ## Multiplication by a scalar test
+        col2 = col1.multByScal(2)
+        check areClose(col2.r, 2.0)
+        check areClose(col2.g, 4.0)
+        check areClose(col2.b, 6.0)
