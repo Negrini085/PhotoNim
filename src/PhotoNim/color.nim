882ff020affc2d22285c1e58ecc373d9f1fd3ddf
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-03-11 19:55:19 +0100

    feat(Color): add multiplication of two colors

--- src/PhotoNim/color.nim
+++ src/PhotoNim/color.nim
@@ -9,7 +9,13 @@ proc newColor*(r, g, b: float32): Color {.inline.} = Color(r: r, g: g, b: b)
 
 
 
-proc multByScalar*(color: Color, scalar: float32) : Color = 
+proc areClose*(x, y: Color): bool {.inline.} = 
+    ## Check if two Color are the same up to numerical precision using areClose
+    areClose(x.r, y.r) and areClose(x.g, y.g) and areClose(x.b, y.b)
+
+
+
+proc multByScal*(color: Color, scalar: float32) : Color = 
     ## Multiplication by a scalar
     result.r = color.r * scalar
     result.g = color.g * scalar
@@ -19,6 +25,8 @@ proc multByScalar*(color: Color, scalar: float32) : Color =
 
 
 
-proc areClose*(x, y: Color): bool {.inline.} = 
-    ## Check if two Color are the same up to numerical precision using areClose
-    areClose(x.r, y.r) and areClose(x.g, y.g) and areClose(x.b, y.b)
+proc multColor*(col1, col2: Color) : Color = 
+    ## Multiplication of two colors
+    result.r = col1.r * col2.r
+    result.g = col1.g * col2.g
+    result.b = col1.b * col2.b
