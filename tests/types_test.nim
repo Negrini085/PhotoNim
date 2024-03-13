6c213ebe5132b93f14d3758572cdaaaab18b4797
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2024-03-13 11:17:30 +0100

    refactor: define different submodules for Color and HdrImage

--- tests/types_test.nim
+++ tests/types_test.nim
@@ -19,7 +19,7 @@ suite "ColorTest":
         check areClose(col1.b, 3.0)
         check areClose(col2, newColor(0.0, 0.0, 0.0))
 
-    test "sum":
+    test "sumColor":
         ## Sum of two color test
         col2 = newColor(0.1, 0.2, 0.3)
         var res: Color = col1 + col2
@@ -28,7 +28,7 @@ suite "ColorTest":
         check areClose(res.g, 2.2)
         check areClose(res.b, 3.3)
 
-    test "mul":
+    test "mulColor":
         ## Multiplication of two colors test
         col2 = col1 * 2.0
         var col3: Color = col1 * col2
@@ -37,7 +37,7 @@ suite "ColorTest":
         check areClose(col3.g, 8.0)
         check areClose(col3.b, 18.0)
 
-    test "scale":
+    test "scaleColor":
         ## Multiplication by a scalar test
         col2 = 2.0 * col1
         check areClose(col2.r, 2.0)
