c2abd33cf688ae2d9d3b59379022a9be2d5277bb
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2024-03-13 10:49:57 +0100

    feat(Types): Color: operators +, * overloaded, HdrImage: improved allocation in newHdrImage, get and set procedures.

--- src/PhotoNim/types.nim
+++ src/PhotoNim/types.nim
@@ -1,50 +1,49 @@
 import common
 
-
-
 #-----------------------------------------------#
 #                  Color type                   #
 #-----------------------------------------------#  
 
 type 
     Color* = object
-        ## A Color in RGB format
+        ## `Color` represents a color in RGB format
         r*, g*, b*: float32
 
-proc newColor*(r, g, b: float32): Color {.inline.} = Color(r: r, g: g, b: b)
-
+proc newColor*(r, g, b: float32): Color {.inline.} = 
+    ## Create a new RGB `Color`
+    Color(r: r, g: g, b: b)
 
 
 proc areClose*(x, y: Color): bool {.inline.} = 
-    ## Check if two Color are the same up to numerical precision using areClose
+    ## Check if two `Color`s are the same up to numerical precision using `areClose`
     areClose(x.r, y.r) and areClose(x.g, y.g) and areClose(x.b, y.b)
 
 
-
-proc sumCol*(col1: Color, col2: Color): Color =
-    ## Sum of two colors 
+proc `+`*(col1: Color, col2: Color): Color =
+    ## Sum of two RGB `Color`s
     result.r = col1.r + col2.r
     result.g = col1.g + col2.g
     result.b = col1.b + col2.b
 
 
-
-proc multByScal*(col: Color, scal: float32): Color = 
-    ## Multiplication by a scalar
+proc `*`*(col: Color, scal: float32): Color = 
+    ## Multiply a `Color` by a scalar
     result.r = col.r * scal
     result.g = col.g * scal
     result.b = col.b * scal
 
+proc `*`*(scal: float32, col: Color): Color {.inline.} = col * scal
 
 
-proc multCol*(col1, col2: Color): Color = 
-    ## Multiplication of two colors
+proc `*`*(col1, col2: Color): Color = 
+    ## Multiply two RGB `Color`s element wise
     result.r = col1.r * col2.r
     result.g = col1.g * col2.g
     result.b = col1.b * col2.b
 
 
-
+const Black* = newColor(0, 0, 0)
+const White* = newColor(255, 255, 255)
 
 
 #-----------------------------------------------#
@@ -53,32 +52,41 @@ proc multCol*(col1, col2: Color): Color =
 
 type
     HdrImage* = object
-        ## An HDR image
+        ## `HdrImage` represents an HDR image as a sequence of `Color` associated with each pixel in (width, height)
         width*, height*: int
         image*: seq[Color]
 
 
-
-proc newHdrImage*(width, height: int): HdrImage = 
-    ## Contructor which creates a one dimensional array of colors, whose dimension is width * height
+proc newHdrImage*(width, height: int, bgColor: Color = Black): HdrImage = 
+    ## Create a black (width, height) `HdrImage` canvas
+    ## ToDo: 
+    ##  - Fill image with bgColor
     result.width = width
     result.height = height
-    
-    ## Every pixel is initialized as black (pixel.r = 0.0, pixel.g = 0.0, pixel.b = 0.0)
-    for i in 0..<width*height:
-        result.image.add(newColor(0.0, 0.0, 0.0))
+    result.image = newSeq[Color](width * height)
 
 
-
-proc valid_coord*(img: HdrImage, row, col: int): bool =
-    ## Checks if given coordinates are valid or not
-    return (row >= 0) and (row < img.width) and (col >= 0) and (col < img.height)
-
+proc valid_coord(img: HdrImage, row, col: int): bool {.inline.} =
+    ## Check if pixel coordinates are valid in a `HdrImage`
+    (row >= 0) and (row < img.width) and (col >= 0) and (col < img.height)
 
 
-proc pixel_offset*(img: HdrImage, row, col: int): int =
-    ## Calculate pixel position in HdrImage.image = seq[Color]
-    return row*img.width + col
+proc pixel_offset(img: HdrImage, row, col: int): int {.inline.} =
+    ## Calculate pixel position in a `HdrImage`
+    row * img.width + col
 
 
+proc get_pixel*(img: HdrImage, row, col: int) : Color = 
+    ## Access the `Color` of pixel (row, col) in a `HdrImage`
+    assert img.valid_coord(row, col)
+    img.image[img.pixel_offset(row, col)]
 
+proc get_pixel*(img: var HdrImage, row, col: int) : var Color = 
+    ## Access the `Color` of pixel (row, col) in a `HdrImage`
+    assert img.valid_coord(row, col)
+    img.image[img.pixel_offset(row, col)]
+
+proc set_pixel*(img: var HdrImage, row, col: int, color: Color) = 
+    ## Set the `Color` of pixel (row, col) in a `HdrImage`
+    assert img.valid_coord(row, col)
+    img.image[img.pixel_offset(row, col)] = color
--- tests/types_test.nim
+++ tests/types_test.nim
@@ -3,7 +3,6 @@ import PhotoNim/common
 import PhotoNim/types
 
 
-
 #-------------------------------------------------------#
 #                   Test of Color type                  #
 #-------------------------------------------------------#
@@ -20,33 +19,30 @@ suite "ColorTest":
         check areClose(col1.b, 3.0)
         check areClose(col2, newColor(0.0, 0.0, 0.0))
 
-    test "sumCol":
+    test "sum":
         ## Sum of two color test
         col2 = newColor(0.1, 0.2, 0.3)
-        var res: Color = col1.sumCol(col2)
+        var res: Color = col1 + col2
 
         check areClose(res.r, 1.1)
         check areClose(res.g, 2.2)
         check areClose(res.b, 3.3)
 
-    test "multByScal":
-        ## Multiplication by a scalar test
-        col2 = col1.multByScal(2)
-        check areClose(col2.r, 2.0)
-        check areClose(col2.g, 4.0)
-        check areClose(col2.b, 6.0)
-
-    test "multCol":
+    test "mul":
         ## Multiplication of two colors test
-        col2 = col1.multByScal(2)
-        var col3: Color = col1.multCol(col2)
+        col2 = col1 * 2.0
+        var col3: Color = col1 * col2
 
         check areClose(col3.r, 2.0)
         check areClose(col3.g, 8.0)
         check areClose(col3.b, 18.0)
 
-
-
+    test "scale":
+        ## Multiplication by a scalar test
+        col2 = 2.0 * col1
+        check areClose(col2.r, 2.0)
+        check areClose(col2.g, 4.0)
+        check areClose(col2.b, 6.0)
 
 
 
@@ -57,54 +53,51 @@ suite "ColorTest":
 suite "HdrImageTest":
     
     setup:
-        var im1: HdrImage = newHdrImage(2, 2)
-        let im2: HdrImage = HdrImage()
+        var img: HdrImage = newHdrImage(2, 2)
     
     test "newHdrImage":
         ## Constructor test
         
         # Checking dimensions (width and height)
-        check areClose(float(im1.width), 2.0)
-        check areClose(float(im1.height), 2.0)
-        check areClose(float(im2.width), 0.0)
-        check areClose(float(im2.height), 0.0)
+        check areClose(float(img.width), 2.0)
+        check areClose(float(img.height), 2.0)
 
         #Checking array content
-        for i in 0..<im1.height*im1.width:
-            check areClose(im1.image[i].r, 0.0)
-            check areClose(im1.image[i].g, 0.0)
-            check areClose(im1.image[i].b, 0.0)
+        for i in 0..<img.height*img.width:
+            check areClose(img.image[i].r, 0.0)
+            check areClose(img.image[i].g, 0.0)
+            check areClose(img.image[i].b, 0.0)
 
-    test "valid_coord":
-        ## Valid coordinates test
+    # test "valid_coord":
+    #     ## Valid coordinates test
         
-        # Testing negative coordinates
-        check not valid_coord(im1, -1, 0)
-        check not valid_coord(im1, 0, -1)
+    #     # Testing negative coordinates
+    #     check not valid_coord(img, -1, 0)
+    #     check not valid_coord(img, 0, -1)
 
-        #Testing out of bound coordinates
-        check not valid_coord(im1, 2, 0)
-        check not valid_coord(im1, 0, 2)
+    #     #Testing out of bound coordinates
+    #     check not valid_coord(img, 2, 0)
+    #     check not valid_coord(img, 0, 2)
 
-        #Testing valid coordinates
-        check valid_coord(im1, 0, 0)
-        check valid_coord(im1, 1, 1)
+    #     #Testing valid coordinates
+    #     check valid_coord(img, 0, 0)
+    #     check valid_coord(img, 1, 1)
 
-    test "pixel_offset":
-        ## Pixel index calculator test
-        check areClose(float(im1.pixel_offset(0,0)), 0.0)
-        check areClose(float(im1.pixel_offset(1,1)), 3)
+    # test "pixel_offset":
+    #     ## Pixel index calculator test
+    #     check areClose(float(img.pixel_offset(0,0)), 0.0)
+    #     check areClose(float(img.pixel_offset(1,1)), 3)
 
     test "get_pixel":
         ## Get pixel test
-        im1.image[3] = newColor(1.0, 2.0, 3.0)
+        img.image[3] = newColor(1.0, 2.0, 3.0)
         
         # Testing black color
-        check areClose(im1.get_pixel(1,0).r, 0.0)
-        check areClose(im1.get_pixel(1,0).g, 0.0)
-        check areClose(im1.get_pixel(1,0).b, 0.0)       
+        check areClose(img.get_pixel(1,0).r, 0.0)
+        check areClose(img.get_pixel(1,0).g, 0.0)
+        check areClose(img.get_pixel(1,0).b, 0.0)       
 
         # Testing non black color
-        check areClose(im1.get_pixel(1,1).r, 1.0)
-        check areClose(im1.get_pixel(1,1).g, 2.0)
-        check areClose(im1.get_pixel(1,1).b, 3.0)
+        check areClose(img.get_pixel(1,1).r, 1.0)
+        check areClose(img.get_pixel(1,1).g, 2.0)
+        check areClose(img.get_pixel(1,1).b, 3.0)
