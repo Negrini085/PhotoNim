6bf18e5549da5f8b221358d850cd289b5bc32184
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2024-03-20 13:09:16 +0100

    "feat(PhotoNim): cli to convert an HDRImage to LDRImage using docopts."

--- PhotoNim.nimble
+++ PhotoNim.nimble
@@ -5,8 +5,9 @@ author        = "lorenzoliuzzo & Negrini085"
 description   = "A new awesome nimble package"
 license       = "GPL-3.0-or-later"
 srcDir        = "src"
-
+bin           = @["PhotoNim"]
 
 # Dependencies
 
 requires "nim >= 2.0.2"
+requires "docopt >= 0.6"
--- src/PhotoNim.nim
+++ src/PhotoNim.nim
@@ -1,1 +1,49 @@
-import PhotoNim/[common, color, hdrimage]
+import strutils
+
+let doc = """
+PhotoNim, a simple CPU raytracer written in Nim.
+
+Usage: 
+    ./PhotoNim convert <HDR> [<LDR>] [--alpha=<alpha> --gamma=<gamma>]
+
+Options:
+    --alpha=<alpha>     Color renormalization factor [default: 0.18]
+    --gamma=<gamma>     LDR factor [default: 1.0]
+    -h --help     
+    --version     
+"""
+
+
+import docopt
+
+let args = docopt(doc, version = "PhotoNim 0.1")
+
+if args["convert"]:
+    let 
+        ifile = args["<HDR>"]
+        ofile = args["<LDR>"]
+    var alpha, gamma: float32
+
+    if args["--alpha"]: 
+        try: alpha = parseFloat($args["--alpha"]) 
+        except: echo "Warning: alpha flag must be a float. Default value is used."
+
+    if args["--gamma"]: 
+        try: gamma = parseFloat($args["--gamma"]) 
+        except: echo "Warning: gamma flag must be a float. Default value is used."
+
+    echo "Converting an HDRImage to a LDRImage."
+
+    echo "ifile ", ifile
+    echo "ofile ", ofile   
+    echo "alpha ", alpha
+    echo "gamma ", gamma   
+
+    quit()
+
+elif args["render"]:
+    quit()
+
+else: 
+    echo "No other commands are availables!"
+    quit()
