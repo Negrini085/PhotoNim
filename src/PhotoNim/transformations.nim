0f502b1941a053f11d345dca59397d57b0e359ec
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2024-04-17 12:20:50 +0200

    refactor: newScaling and newTranslation.

--- src/PhotoNim/transformations.nim
+++ src/PhotoNim/transformations.nim
@@ -21,8 +21,25 @@ proc `*`*(T: Transformation, scal: float32): Transformation {.inline.} = newTran
 proc `*`*(scal: float32, T: Transformation): Transformation {.inline.} = newTransformation(scal * T.mat, scal * T.inv_mat)
 proc `/`*(T: Transformation, scal: float32): Transformation {.inline.} = newTransformation(T.mat / scal, T.inv_mat / scal)
 
-proc newScaling(vec: Vec3f): Scaling =
-    quit "to overload"
+proc newScaling(scal: float32): Scaling =
+    result.mat = scal * Mat4f.id; 
+    result.inv_mat = Mat4f.id / scal
+    result.mat[3][3] = 1.0
+    result.inv_mat[3][3] = 1.0
+
+proc newTranslation*(v: Vec4f): Translation  = 
+    result.mat = [
+        [1, 0, 0, v[0]], 
+        [0, 1, 0, v[1]], 
+        [0, 0, 1, v[2]], 
+        [0, 0, 0, 1   ]
+    ]
+    result.inv_mat = [
+        [1, 0, 0, -v[0]], 
+        [0, 1, 0, -v[1]], 
+        [0, 0, 1, -v[2]], 
+        [0, 0, 0, 1    ]   
+    ]
 
 method apply(T: Transformation, a: Vec4f): Vec4f {.base, inline.} = T @ a
 method apply(T: Scaling, a: Vec4f): Vec4f {.inline.} = 
