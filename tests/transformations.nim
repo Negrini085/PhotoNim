8558bf1491e10054c1f07da9dfec887770b8bbdd
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-04-19 12:54:00 +0200

    test(Transformation): add mult/div by a scalar test

--- tests/transformations.nim
+++ tests/transformations.nim
@@ -5,15 +5,15 @@ import PhotoNim/[transformations, common]
 #     Transformation type test     #
 #----------------------------------#
 suite "Transformation tests":
-
-    test "Transformation product test":
-        var
-            t1, t2, ris: Transformation
+    var
             mat: Mat4f = [[1, 0, 0, 4], [0, 1, 0, 3], [0, 0, 1, -1], [0, 0, 0, 1]]
             inv_mat: Mat4f = [[1, 0, 0, -4], [0, 1, 0, -3], [0, 0, 1, 1], [0, 0, 0, 1]]
+            t1: Transformation = newTransformation(mat, inv_mat)
+
+    test "Transformation product test":
+        var t2, ris: Transformation
         
         # Are transformations consistent?
-        t1 = newTransformation(mat, inv_mat)
         t2 = newTransformation(inv_mat, mat)
         check t1.is_consistent() and t2.is_consistent()
 
@@ -27,5 +27,39 @@ suite "Transformation tests":
         check areClose(ris.mat, Mat4f.id)
         check areClose(ris.inv_mat, Mat4f.id)
         
-    test "Transformation application test"
 
+    test "Transformation application test":
+        var
+            vec: Vec4f = newVec4[float32](1, 2, 3, 0)
+            point: Vec4f = newVec4[float32](1, 2, 3, 1)
+            ris: Vec4f
+
+        # In order to test general methods we are using translation matrices: that means that
+        # transformation acts different depending on the last vector component
+            
+        ris = t1 @ vec; point = t1 @ point
+        check areClose(ris, vec)
+        check areClose(point, newVec4[float32](5, 5, 2, 1))
+    
+
+    test "Transformation mult/div by scalar":
+        var
+            scal: float32 = 2.0
+            m1: Mat4f = [[2, 0, 0, 8], [0, 2, 0, 6], [0, 0, 2, -2], [0, 0, 0, 2]]
+            m2: Mat4f = [[2, 0, 0, -8], [0, 2, 0, -6], [0, 0, 2, 2], [0, 0, 0, 2]]
+        
+        t1 = 2 * t1;
+        check areClose(t1.mat, m1)
+        check areClose(t1.inv_mat, m2)
+
+        t1 = t1/2
+        check areClose(t1.mat, mat)
+        check areClose(t1.inv_mat, inv_mat)
+
+        t1 = t1 * 2
+        check areClose(t1.mat, m1)
+        check areClose(t1.inv_mat, m2)
+
+
+        
+        
