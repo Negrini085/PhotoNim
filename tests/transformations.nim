a5e4984897dd45d6ac2f33240a86eecf651a98ef
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-04-19 15:24:43 +0200

    test(Transformation): add Rotation test

--- tests/transformations.nim
+++ tests/transformations.nim
@@ -108,3 +108,17 @@ suite "Derived Transformation test":
         check areClose(t.apply(vec), vec)
         check areClose(t.apply(point), newVec4[float32](3, 4, 4, 1))
 
+    test "Rotation":
+        var
+            tx: Rotation = newRotX(180) 
+            ty: Rotation = newRotY(180) 
+            tz: Rotation = newRotZ(180) 
+            vec: Vec4f = newVec4[float32](1, 2, 3, 1)
+
+        check tx.is_consistent()
+        check ty.is_consistent()
+        check tz.is_consistent()
+
+        check areClose(tx @ vec, newVec4[float32](1.0, -2.0, -3.0, 1.0))
+        check areClose(ty @ vec, newVec4[float32](-1.0, 2.0, -3.0, 1.0))
+        check areClose(tz @ vec, newVec4[float32](-1.0, -2.0, 3.0, 1.0))
