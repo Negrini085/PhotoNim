c81a8ae7a20228e81801483351aaa1d4b16b01fc
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-04-19 10:04:08 +0200

    feat(Translation): add traslation application optimized method

--- src/PhotoNim/transformations.nim
+++ src/PhotoNim/transformations.nim
@@ -7,6 +7,7 @@ type Transformation* = object of RootObj
 proc newTransformation*(mat, inv_mat: Mat4f): Transformation = 
     (result.mat, result.inv_mat) = (mat, inv_mat)
 
+
 type Translation = object of Transformation
 type Scaling = object of Transformation
 type Rotation = object of Transformation
@@ -16,17 +17,21 @@ proc `@`*(a, b: Transformation): Transformation =
     result.mat = dot(a.mat, b.mat)
     result.inv_mat = dot(b.inv_mat, a.inv_mat)
     
+
 proc `@`*(a: Transformation, b: Vec4f): Vec4f =
     ## Procedure to apply a transformation
     result = dot(a.mat, b)
+
 
 proc `*`*(T: Transformation, scal: float32): Transformation {.inline.} = newTransformation(scal * T.mat, scal * T.inv_mat)
 proc `*`*(scal: float32, T: Transformation): Transformation {.inline.} = newTransformation(scal * T.mat, scal * T.inv_mat)
 proc `/`*(T: Transformation, scal: float32): Transformation {.inline.} = newTransformation(T.mat / scal, T.inv_mat / scal)
 
+
 proc inverse(T: Transformation): Transformation {.inline.} =
     ## Procedure to get the inverse Transformation
     result.mat = T.inv_mat; result.inv_mat = T.mat
+
 
 proc is_consistent*(t1: Transformation): bool = 
     ## Checks whether the transformation is consistent or not: product within matrix and inverse gives identity??
@@ -34,12 +39,14 @@ proc is_consistent*(t1: Transformation): bool =
     echo Mat4f.id
     result = areClose(dot(t1.mat, t1.inv_mat), Mat4f.id)
 
+
 proc newScaling(scal: float32): Scaling =
     ## Procedure to define a new scaling transformation
     result.mat = scal * Mat4f.id; 
     result.inv_mat = Mat4f.id / scal
     result.mat[3][3] = 1.0
     result.inv_mat[3][3] = 1.0
+
 
 proc newTranslation*(v: Vec4f): Translation  = 
     ## Procedure to define a new scaling transformation
@@ -56,9 +63,15 @@ proc newTranslation*(v: Vec4f): Translation  =
         [0, 0, 0, 1    ]   
     ]
 
+
 method apply(T: Transformation, a: Vec4f): Vec4f {.base, inline.} = T @ a
     ## Method to apply a generic transformation
 
 method apply(T: Scaling, a: Vec4f): Vec4f {.inline.} = 
     ## Method to apply a scaling transformation
     result[0] = T.mat[0][0] * a[0]; result[1] = T.mat[1][1] * a[1]; result[2] = T.mat[2][2] * a[2]; result[3] = a[3]; 
+
+method apply(T: Translation, a: Vec4f): Vec4f =
+    ## Method to apply a translation transformation
+    result[0] = a[0] + T.mat[0][3] * a[3]; result[1] = a[1] + T.mat[1][3] * a[3]; 
+    result[2] = a[2] + T.mat[2][3] * a[3]; result[3] = a[3];
