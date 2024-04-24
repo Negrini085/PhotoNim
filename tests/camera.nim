ef460fabd73e0fdc099d23dfc33f4d8d0a72ecc4
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-04-24 23:08:04 +0200

    test(Ray): add ray translation test

--- tests/camera.nim
+++ tests/camera.nim
@@ -1,8 +1,6 @@
 import std/[unittest, math]
 import PhotoNim/[transformations, common, geometry, camera]
 
-#import PhotoNim/[transformations, common, geometry, camera]
-
 #----------------------------------#
 #          Ray type tests          #
 #----------------------------------#
@@ -10,12 +8,12 @@ suite "Ray tests":
 
     var ray = newRay(newPoint3D(1, 2, 3), newVec3[float32](1, 0, 0))
 
-    test "Ray constructor":
+    test "newRay":
         # Checking constructor test
         check areClose(ray.start, newPoint3D(1, 2, 3))
         check areClose(ray.dir, newVec3[float32](1, 0, 0))
     
-    test "At procedure":
+    test "at":
         # Checking at procedure
         check areClose(ray.at(2.0), newPoint3D(3, 2, 3))
 
@@ -27,3 +25,9 @@ suite "Ray tests":
 
         check areClose(ray, ray1)
         check not areClose(ray, ray2)
+    
+    test "translateRay":
+        # Checking ray translation procedures
+        var vec = newVec3[float32](1, 2, 3)
+
+        check areClose(ray.translateRay(vec).start, newPoint3D(2, 4, 6))
