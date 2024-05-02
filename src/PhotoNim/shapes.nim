5d7cef32af9ba776f3d1728ede9905961f13b820
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-05-02 18:40:35 +0200

    feat(Shapes): add Shape abstract type and base method intersectionRay

--- src/PhotoNim/shapes.nim
+++ src/PhotoNim/shapes.nim
@@ -1,5 +1,9 @@
 import common, transformations, camera, geometry
 
+
+#-------------------------------#
+#        Hit Record Type        #
+#-------------------------------#
 type HitRecord* = object
     world_point*: Point3D
     normal*: Normal
@@ -15,4 +19,22 @@ proc newHitRecord*(hit_point: Point3D, norm: Normal, uv: Vec2f, t: float32, ray:
 proc areClose*(hit1, hit2: HitRecord): bool {.inline.} = 
     ## Procedure to test wether two HitRecord are close or not
     return areClose(hit1.world_point, hit2.world_point) and areClose(hit1.normal, hit2.normal) and areClose(hit1.uv, hit2.uv) and areClose(hit1.t, hit2.t) and areClose(hit1.ray, hit2.ray)
+
+
+
+#------------------------------#
+#         Shape types          #
+#------------------------------#
+type Shape* = object of RootObj
+    T*: Transformation
+
+method intersectionRay(shape: Shape, ray: Ray): HitRecord {.base} =
+    ## Base procedure to compute ray intersection with a generic shape
+    quit "to overload"
+    
+
+type
+    Sphere* = object of Shape
+    Plane* = object of Shape
+
 
--- tests/shapes.nim
+++ tests/shapes.nim
@@ -1,12 +1,17 @@
 import std/unittest
 import PhotoNim/[shapes, geometry, camera, common]
 
+#---------------------------------------#
+#         Hit Record type tests         #
+#---------------------------------------#
+
 suite "HitRecord":
 
     setup:
         var 
             hit1 = newHitRecord(newPoint3D(1, 2, 3), newNormal(1, 0, 0), newVec2[float32](1, 0), 0.5, newRay(newPoint3D(0, 0, 0), newVec3[float32](0, 1, 0)))
             hit2 = newHitRecord(newPoint3D(1, 0, 0), newNormal(0, 1, 0), newVec2[float32](0.5, 0.5), 0.6, newRay(newPoint3D(0, 0, 2), newVec3[float32](1, 1, 0)))
+
 
     test "newHitRecord":
         # Checking newHitRecord procedure
@@ -15,7 +20,8 @@ suite "HitRecord":
         check areClose(hit1.normal, newNormal(1, 0, 0))
         check areClose(hit1.t, 0.5)
         check areClose(hit1.ray, newRay(newPoint3D(0, 0, 0), newVec3[float32](0, 1, 0)))
-    
+
+
     test "areClose":
         # Checking areClose procedure for HitRecord variables
         check areClose(hit1, hit1)
@@ -25,5 +31,5 @@ suite "HitRecord":
         check not areClose(hit1.ray, hit2.ray)
         check not areClose(hit1.uv, hit2.uv)
         check not areClose(hit1.t, hit2.t)
-        
+
         check not areClose(hit1, hit2)
