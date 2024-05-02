e1bf6fde2d05cc4cae9e3b9d8e6bcf63d22628f0
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-05-02 18:17:56 +0200

    refactor(HitRecordTest): changed newHitRecord procedure test

--- tests/shapes.nim
+++ tests/shapes.nim
@@ -5,8 +5,8 @@ suite "HitRecord":
 
     setup:
         var 
-            hit1 = newHitRecord(newPoint3D(1, 2, 3), newVec2[float32](1, 0), newNormal(1, 0, 0), 0.5, newRay(newPoint3D(0, 0, 0), newVec3[float32](0, 1, 0)))
-            hit2 = newHitRecord(newPoint3D(1, 0, 0), newVec2[float32](0.5, 0.5), newNormal(0, 1, 0), 0.6, newRay(newPoint3D(0, 0, 2), newVec3[float32](1, 1, 0)))
+            hit1 = newHitRecord(newPoint3D(1, 2, 3), newNormal(1, 0, 0), newVec2[float32](1, 0), 0.5, newRay(newPoint3D(0, 0, 0), newVec3[float32](0, 1, 0)))
+            hit2 = newHitRecord(newPoint3D(1, 0, 0), newNormal(0, 1, 0), newVec2[float32](0.5, 0.5), 0.6, newRay(newPoint3D(0, 0, 2), newVec3[float32](1, 1, 0)))
 
     test "newHitRecord":
         # Checking newHitRecord procedure
