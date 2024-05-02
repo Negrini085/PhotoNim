c376f92c7449e098fca46b43d411f1ab270f32de
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-05-02 17:19:55 +0200

    feat(HitRecord): add HitRecord type

--- src/PhotoNim/shape.nim
+++ src/PhotoNim/shape.nim
@@ -1,1 +1,11 @@
-import common
+import common, transformations, camera, geometry
+
+type HitRecord* = object
+    world_point*: Point3D
+    surf_point*: Vec2f
+    normal*: Normal
+    t*: float32
+    ray*: Ray
+    
+
+    
