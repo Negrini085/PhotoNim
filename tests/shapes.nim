8a600ff155fb3ff2800100aef4252e56bd132501
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-05-03 14:06:43 +0200

    test(Sphere): add first module intersectionRay procedure test

--- tests/shapes.nim
+++ tests/shapes.nim
@@ -1,4 +1,4 @@
-import std/[unittest, math]
+import std/[unittest, math, options]
 import PhotoNim/[shapes, geometry, camera, common, transformations]
 
 #---------------------------------------#
@@ -67,6 +67,29 @@ suite "Sphere":
         var
             p1 = newPoint3D(1, 0, 1)
             p2 = newPoint3D(cos(PI/3), sin(PI/3), 0.5)
-            
+
         check areClose(sphere_uv(p1), newVec2[float32](0, 0))
         check areClose(sphere_uv(p2), newVec2[float32](1/6, 1/3))
+    
+
+    test "RayIntersection: module 1":
+        # Checking ray intersection procedure
+        var
+            ray1 = newRay(newPoint3D(0, 0, 2), newVec3[float32](0, 0, -1))
+            ray2 = newRay(newPoint3D(3, 0, 0), newVec3[float32](-1, 0, 0))
+            ray3 = newRay(newPoint3D(0, 0, 0), newVec3[float32](1, 0, 0))
+
+        check areClose(sphere.intersectionRay(ray1).get().world_point, newPoint3D(0, 0, 1))
+        check areClose(sphere.intersectionRay(ray1).get().normal, newNormal(0, 0, 1))
+        check areClose(sphere.intersectionRay(ray1).get().t, 1)
+        check areClose(sphere.intersectionRay(ray1).get().uv, newVec2[float32](0, 0))
+
+        check areClose(sphere.intersectionRay(ray2).get().world_point, newPoint3D(1, 0, 0))
+        check areClose(sphere.intersectionRay(ray2).get().normal, newNormal(1, 0, 0))
+        check areClose(sphere.intersectionRay(ray2).get().t, 2)
+        check areClose(sphere.intersectionRay(ray1).get().uv, newVec2[float32](0, 0))
+
+        check areClose(sphere.intersectionRay(ray3).get().world_point, newPoint3D(1, 0, 0))
+        check areClose(sphere.intersectionRay(ray3).get().normal, newNormal(-1, 0, 0))
+        check areClose(sphere.intersectionRay(ray3).get().t, 1)
+        check areClose(sphere.intersectionRay(ray1).get().uv, newVec2[float32](0, 0))
