d65358a84b63c763506f6a2586a9bef8b2ecfae6
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2024-05-08 15:41:17 +0200

    refactor(camera): newCamera procs are now divided in newOrthogonalCamera and newPerspectiveCamera.

--- src/PhotoNim/camera.nim
+++ src/PhotoNim/camera.nim
@@ -1,7 +1,5 @@
 from std/fenv import epsilon 
-
-import geometry
-import hdrimage
+import geometry, hdrimage
 
 
 type
@@ -20,22 +18,23 @@ type
         tmax*: float32
         depth*: int
 
+    ImageTracer* = object
+        image*: HdrImage
+        camera*: Camera
 
-proc newCamera*(a: float32; transf = Transformation.id): OrthogonalCamera {.inline.} = 
+
+proc newOrthogonalCamera*(a: float32; transf = Transformation.id): OrthogonalCamera {.inline.} = 
     OrthogonalCamera(aspect_ratio: a, transf: transf)
 
-proc newCamera*(a, d: float32; transf = Transformation.id): PerspectiveCamera {.inline.} = 
+proc newPerspectiveCamera*(a, d: float32; transf = Transformation.id): PerspectiveCamera {.inline.} = 
     PerspectiveCamera(aspect_ratio: a, distance: d, transf: transf)
-
 
 proc newRay*(start: Point3D, direction: Vec3f): Ray {.inline} = 
     Ray(start: start, dir: direction, tmin: 1e-5, tmax: Inf, depth: 0)  
 
-proc translateRay*(ray: Ray, vec: Vec3f): Ray {.inline.} = 
-    Ray(start: ray.start + vec, dir: ray.dir, tmin: ray.tmin, tmax: ray.tmax, depth: ray.depth)
+proc newImageTracer*(im: HdrImage, cam: Camera): ImageTracer {.inline.} = 
+    ImageTracer(image: im, camera: cam)
 
-proc transformRay*(transf: Transformation, ray: Ray): Ray {.inline.} =
-    Ray(start: apply(transf, ray.start), dir: apply(transf, ray.dir), tmin: ray.tmin, tmax: ray.tmax, depth: ray.depth)
 
 proc at*(ray: Ray, time: float32): Point3D {.inline.} = ray.start + ray.dir * time
 
@@ -43,29 +42,27 @@ proc areClose*(a, b: Ray; eps: float32 = epsilon(float32)): bool {.inline} =
     areClose(a.start, b.start, eps) and areClose(a.dir, b.dir, eps)
 
 
-method fire_ray*(cam: Camera, pix_pt: Point2D): Ray {.base.} =
+method apply*(transf: Transformation, ray: Ray): Ray {.base, inline.} =
+    Ray(start: apply(transf, ray.start), dir: apply(transf, ray.dir), tmin: ray.tmin, tmax: ray.tmax, depth: ray.depth)
+
+proc translate*(ray: Ray, vec: Vec3f): Ray {.inline.} = 
+    Ray(start: ray.start + vec, dir: ray.dir, tmin: ray.tmin, tmax: ray.tmax, depth: ray.depth)
+
+
+method fire_ray*(cam: Camera, pixel: Point2D): Ray {.base.} =
     quit "to overload"
 
-method fire_ray*(cam: OrthogonalCamera, pix_pt: Point2D): Ray {.inline.} = 
-    let origin = newPoint3D(-1, (1 - 2 * pix_pt.u) * cam.aspect_ratio, 2 * pix_pt.v - 1)
-    transformRay(cam.transf, newRay(origin, eX))
+method fire_ray*(cam: OrthogonalCamera, pixel: Point2D): Ray {.inline.} = 
+    apply(cam.transf, newRay(newPoint3D(-1, (1 - 2 * pixel.u) * cam.aspect_ratio, 2 * pixel.v - 1), eX))
 
-method fire_ray*(cam: PerspectiveCamera, pix_pt: Point2D): Ray {.inline.} = 
-    let origin = newPoint3D(-cam.distance, 0, 0)
-    let dir = newVec3(cam.distance, (1 - 2 * pix_pt.u) * cam.aspect_ratio, 2 * pix_pt.v - 1)
-    transformRay(cam.transf, newRay(origin, dir))
+method fire_ray*(cam: PerspectiveCamera, pixel: Point2D): Ray {.inline.} = 
+    apply(cam.transf, newRay(newPoint3D(-cam.distance, 0, 0), newVec3(cam.distance, (1 - 2 * pixel.u) * cam.aspect_ratio, 2 * pixel.v - 1)))
 
 
 
-type ImageTracer* = object
-    image*: HdrImage
-    camera*: Camera
-
-proc newImageTracer*(im: HdrImage, cam: Camera): ImageTracer {.inline.} = ImageTracer(image: im, camera: cam)
-
-proc fire_ray*(im_tr: ImageTracer, col, row: int, pix_pt: Point2D = newPoint2D(0.5, 0.5)): Ray =
-    let u = (col.toFloat + pix_pt.u) / im_tr.image.width.toFloat
-    let v = 1 - (row.toFloat + pix_pt.v) / im_tr.image.height.toFloat
+proc fire_ray*(im_tr: ImageTracer, col, row: int, pixel: Point2D = newPoint2D(0.5, 0.5)): Ray =
+    let u = (col.toFloat + pixel.u) / im_tr.image.width.toFloat
+    let v = 1 - (row.toFloat + pixel.v) / im_tr.image.height.toFloat
     
     im_tr.camera.fire_ray(newPoint2D(u, v))
 
