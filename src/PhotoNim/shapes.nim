63e89dbd2e733b0c3e9ef0137187c59fdce51295
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2024-05-16 00:14:20 +0200

    feat: rayIntersection for Triangle. ToDo: implement solve proc for matrix equations.

--- src/PhotoNim/shapes.nim
+++ src/PhotoNim/shapes.nim
@@ -201,6 +201,25 @@ proc rayIntersection*[S: Shape](shape: S, ray: Ray): Option[HitRecord] =
     
     some(HitRecord(ray: ray, t_hit: t_hit, world_pt: apply(shape.transf, hit_pt), surface_pt: shape.uv(hit_pt), normal: apply(shape.transf, normal).normalize))
 
+proc rayIntersection*(triangle: Triangle, ray: Ray): Option[HitRecord] =
+    let 
+        (A, B, C) = triangle.vertices
+        min = [ray.origin.x - A.x, ray.origin.y - A.y, ray.origin.z - A.z]
+        max = [
+            [B.x-A.x, C.x-A.x, -ray.dir[0]], 
+            [B.y-A.y, C.y-A.y, -ray.dir[1]], 
+            [B.z-A.z, C.z-A.z, -ray.dir[2]]
+        ]
+        
+        w = solve(min.T, max.T)
+        (u, v, t_hit) = (w[0], w[1], w[2])
+
+    if ray.tmin > t_hit or t_hit > ray.tmax: return none(HitRecord)
+    if u < 0.0 or v < 0.0 or u > 1 or v > 1: return none(HitRecord)
+
+    let hit_pt = ray.at(t_hit)
+    some(HitRecord(ray: ray, t_hit: t_hit, world_pt: hit_pt, surface_pt: newPoint2D(u, v), normal: triangle.normal(hit_pt, ray.dir)))
+
 
 type 
     Mesh = object of Shape
