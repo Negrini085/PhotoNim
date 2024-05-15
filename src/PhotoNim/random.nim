e3bd7eb497722022f702ca70fc2e0a98447d03e6
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-05-15 12:30:52 +0200

    feat(Pcg): add newPcg procedure

--- src/PhotoNim/random.nim
+++ src/PhotoNim/random.nim
@@ -1,5 +1,11 @@
-import math
-
 type Pcg* = object 
     state*: uint64
     inc*: uint64
+
+proc newPcg*(in_state: uint64 = 42, in_seq: uint64 = 54): Pcg = 
+    # Pcg type constructor
+    result.state = 0; result.inc = ((in_seq shl 1) or 1)
+    discard result.random()
+    result.state += in_state
+    discard result.random()
+
