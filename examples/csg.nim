e1a4152d219e66750f97465b71ebd2338686c26a
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-05-19 22:40:15 +0200

    refactor(CSGUnionEx): correct CSGUnion example

--- examples/csg.nim
+++ examples/csg.nim
@@ -5,12 +5,25 @@ from std/times import cpuTime
 from std/strformat import fmt
 from math import sqrt, exp, pow
 
+proc col_pix(im_tr: ImageTracer, ray: Ray, scenary: World, x, y: int): Color = 
+    # Procedure to decide pixel color (it could be useful to check if scenary len is non zero)
+    let dim = scenary.shapes.len
+    if dim == 0: return newColor(0, 0, 0)
+    for i in 0..<dim:
+        if fastIntersection(scenary.shapes[i], ray): 
+            let 
+                r = (1 - exp(-float32(x + y)))
+                g = y/im_tr.image.height
+                b = pow((1 - x/im_tr.image.width), 2.5)
+            return newColor(r, g, b)
+    
+
 let 
     timeStart = cpuTime()
     (width, height) = (1600, 1000)
     filePFM = "images/csg.pfm"
 
-    # Shapes to add to world
+    # Shapes to add to CSGUnion
     s1 = newSphere(newPoint3D(0, 0, 0), 0.5)
     s2 = newSphere(newPoint3D(0.5, 0, 0), 0.2)
     s3 = newSphere(newPoint3D(-0.5, 0, 0), 0.2)
@@ -25,26 +38,15 @@ var
         camera: newPerspectiveCamera(width / height, 1.0, newTranslation(newVec3(float32 -1, 0, 0)))
     )
     world = newWorld()
+    union = newCSGUnion()
 
-world.shapes.add(s1); world.shapes.add(s2); world.shapes.add(s3);
-world.shapes.add(s4); world.shapes.add(s5); world.shapes.add(s6); 
-world.shapes.add(s7);
+union.shapes.add(s1); union.shapes.add(s2); union.shapes.add(s3);
+union.shapes.add(s4); union.shapes.add(s5); union.shapes.add(s6);
+union.shapes.add(s7);
 
-proc fire_all_union*(tracer: var ImageTracer; scenary: World) = 
-    for y in 0..<tracer.image.height:
-        for x in 0..<tracer.image.width:
+world.shapes.add(union);
 
-            # Coloring pixel based on wether ray intersects or not
-            if unionCSG(world.shapes, tracer.fire_ray(x, y)).isNone:
-                tracer.image.setPixel(x, y, newColor(0, 0, 0))
-            else:
-                let
-                    r = (1 - exp(-float32(x + y))) * 255
-                    g = y/tracer.image.height * 255
-                    b = pow((1 - x / tracer.image.width), 2.5) * 255
-                tracer.image.setPixel(x, y, newColor(r, g, b))
-
-tracer.fire_all_union(world)
+tracer.fire_all_rays(world, col_pix)
 
 var stream = newFileStream(filePFM, fmWrite)
 stream.writePFM(tracer.image)
