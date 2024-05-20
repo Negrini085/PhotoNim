0cee01bed37fe5897e56ecd6df6875bd91934f21
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2024-05-20 17:19:29 +0200

    test(Pigment): tested newUniformPigment, newTexturePigment, newCheckeredPigment.

--- tests/camera.nim
+++ tests/camera.nim
@@ -257,7 +257,7 @@ suite "Camera tests":
 #------------------------------------------#
 #         Image Tracer type tests          #
 #------------------------------------------#
-suite "ImageTracer":
+suite "ImageTracer unittest":
 
     setup:
         var 
@@ -296,3 +296,41 @@ suite "ImageTracer":
                     b = pow((1 - x/im_tr.image.width), 2.5)
                 check areClose(im_tr.image.getPixel(x, y), newColor(r, g, b))
     
+
+
+suite "Pigment unittest":
+
+    setup: 
+        let
+            color1 = newColor(1.0, 2.0, 3.0)
+            color2 = newColor(2.0, 3.0, 1.0)
+            color3 = newColor(2.0, 1.0, 3.0)
+            color4 = newColor(3.0, 2.0, 1.0)
+
+    teardown:
+        discard color1; discard color2; discard color3; discard color4
+
+    test "newUniformPigment proc":
+        let pigment = newUniformPigment(color1)
+        check areClose(pigment.getColor(newPoint2D(0.0, 0.0)), color1)
+        check areClose(pigment.getColor(newPoint2D(1.0, 0.0)), color1)
+        check areClose(pigment.getColor(newPoint2D(0.0, 1.0)), color1)
+        check areClose(pigment.getColor(newPoint2D(1.0, 1.0)), color1)
+    
+    test "newTexturePigment proc":
+        var image = newHDRImage(2, 2)
+        image.setPixel(0, 0, color1); image.setPixel(1, 0, color2)
+        image.setPixel(0, 1, color3); image.setPixel(1, 1, color4)
+
+        let pigment = newTexturePigment(image)
+        check areClose(pigment.getColor(newPoint2D(0.0, 0.0)), color1)
+        check areClose(pigment.getColor(newPoint2D(1.0, 0.0)), newColor(2.0, 3.0, 1.0))
+        check areClose(pigment.getColor(newPoint2D(0.0, 1.0)), newColor(2.0, 1.0, 3.0))
+        check areClose(pigment.getColor(newPoint2D(1.0, 1.0)), newColor(3.0, 2.0, 1.0))
+    
+    test "newCheckeredPigment proc":
+        let pigment = newCheckeredPigment(color1, color2, 2)
+        check areClose(pigment.getColor(newPoint2D(0.25, 0.25)), color1)
+        check areClose(pigment.getColor(newPoint2D(0.75, 0.25)), color2)
+        check areClose(pigment.getColor(newPoint2D(0.25, 0.75)), color2)
+        check areClose(pigment.getColor(newPoint2D(0.75, 0.75)), color1)
