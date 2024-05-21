537fa6acad11d3361721715192dfb572b6509297
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2024-05-21 12:55:34 +0200

    feat(PCG): old rand proc is now called random, new rand proc now return a float32 in [0, 1] range.

--- src/pcg.nim
+++ src/pcg.nim
@@ -2,7 +2,7 @@ type PCG* = object
     state*: uint64
     inc*: uint64
 
-proc rand*(gen: var PCG): uint32 =
+proc random*(gen: var PCG): uint32 =
     # Random number generation procedure
     var 
         oldstate = gen.state
@@ -12,9 +12,13 @@ proc rand*(gen: var PCG): uint32 =
     gen.state = oldstate * uint64(6364136223846793005) + gen.inc
     result = ((xorshift shr rot) or (xorshift shl ((-rot) and 31)))
 
+proc rand*(pcg: var PCG): float32 =
+    ## Returns a new random number uniformly distributed over [0, 1]
+    pcg.random.float32 / 0xffffffff.float32
+
 proc newPCG*(in_state: uint64 = 42, in_seq: uint64 = 54): PCG = 
     # PCG type constructor
     result.state = 0; result.inc = ((in_seq shl 1) or 1)
-    discard result.rand()
+    discard result.random
     result.state += in_state
-    discard result.rand()
+    discard result.random
