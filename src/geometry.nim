8a570ca4675e28f6e25d31fb27dc9df7d46d5883
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2024-05-27 11:30:09 +0200

    feat(Vec): `==` proc.

--- src/geometry.nim
+++ src/geometry.nim
@@ -24,6 +24,12 @@ proc newVec2f*(x, y: float32): Vec2f {.inline.} = newVec([x, y])
 proc newVec3f*(x, y, z: float32): Vec3f {.inline.} = newVec([x, y, z])
 proc newVec4f*(x, y, z, w: float32): Vec4f {.inline.} = newVec([x, y, z, w])
 
+
+proc `==`*[N: static[int], V](a, b: Vec[N, V]): bool =
+    for i in 0..<N:
+        if a[i] != b[i]: return false
+    true
+
 proc areClose*(x, y: float32; eps: float32 = epsilon(float32)): bool {.inline.} = abs(x - y) < eps
 proc areClose*[N: static[int]](a, b: Vec[N, float32]; eps: float32 = epsilon(float32)): bool = 
     for i in 0..<N: 
