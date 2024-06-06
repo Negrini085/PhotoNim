01f0c606e7e4a525948f0318e626053f176c2d19
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-06-06 21:20:21 +0200

    test(Scene): add features to Scene testing

--- tests/scene.nim
+++ tests/scene.nim
@@ -45,22 +45,27 @@ suite "Scene unittest":
         let 
             handler = (newTriangle(newPoint3D(0, -2, 0), newPoint3D(2, 1, 1), newPoint3D(0, 3, 0)), IDENTITY)
             worldRefSystem = newReferenceSystem(ORIGIN3D, stdONB)
-            otherRefSystem = newReferenceSystem(newPoint3D(5.0, 0.0, 0.0), newONB(-eX, eY, -eZ))
-            oRefSystem = newReferenceSystem(newPoint3D(5.0, 0.0, 0.0), stdONB)
+            firstRefSystem = newReferenceSystem(newPoint3D(5.0, 0.0, 0.0), newONB(-eX, eY, -eZ))
+            secondRefSystem = newReferenceSystem(newPoint3D(5.0, 0.0, 0.0), stdONB)
     
-            worldAABB = handler.getWorldAABB
-            otherAABB = otherRefSystem.getAABB(handler)
-            pAABB = oRefSystem.getAABB(handler)
+            worldAABB1 = handler.getWorldAABB
+            worldAABB2 = worldRefSystem.getAABB(handler)
+
+            firstAABB = firstRefSystem.getAABB(handler)
+            secondAABB = secondRefSystem.getAABB(handler)
 
 
-        check areClose(worldAABB.min, newPoint3D(0, -2, 0))
-        check areClose(worldAABB.max, newPoint3D(2, 3, 1))
+        check areClose(worldAABB1.min, newPoint3D(0, -2, 0))
+        check areClose(worldAABB1.max, newPoint3D(2, 3, 1))
 
-        check areClose(otherAABB.min, newPoint3D(3, -2, -1))
-        check areClose(otherAABB.max, newPoint3D(5, 3, 0))
+        check areClose(worldAABB2.min, newPoint3D(0, -2, 0))
+        check areClose(worldAABB2.max, newPoint3D(2, 3, 1))
 
-        check areClose(pAABB.min, newPoint3D(-5, -2, 0))
-        check areClose(pAABB.max, newPoint3D(-3, 3, 1))
+        check areClose(firstAABB.min, newPoint3D(3, -2, -1))
+        check areClose(firstAABB.max, newPoint3D(5, 3, 0))
+
+        check areClose(secondAABB.min, newPoint3D(-5, -2, 0))
+        check areClose(secondAABB.max, newPoint3D(-3, 3, 1))
         
 
     test "getAABB proc":
@@ -68,6 +73,13 @@ suite "Scene unittest":
             handler1 = (newTriangle(newPoint3D(0, -2, 0), newPoint3D(2, 1, 1), newPoint3D(0, 3, 0)), newTranslation([float32 1, 1, -2]))
             handler2 = (newTriangle(newPoint3D(0, -2, 0), newPoint3D(2, 1, 1), newPoint3D(0, 3, 0)), IDENTITY)
             worldRefSystem = newReferenceSystem(ORIGIN3D, stdONB)
-            otherRefSystem = newReferenceSystem(newPoint3D(5.0, 0.0, 0.0), newONB(-eX, eY, -eZ))
+            firstRefSystem = newReferenceSystem(newPoint3D(5.0, 0.0, 0.0), newONB(-eX, eY, -eZ))
         
-            sceneAABB = otherRefSystem.getAABB(@[handler1, handler2])
+            worldRefAABB = worldRefSystem.getAABB(@[handler1, handler2])
+            firstRefAABB = firstRefSystem.getAABB(@[handler1, handler2])
+
+        check areClose(worldRefAABB.min.Vec3f, newVec3f(0, -2, -2))
+        check areClose(worldRefAABB.max.Vec3f, newVec3f(3, 4, 1))
+
+        check areClose(firstRefAABB.min.Vec3f, newVec3f(2, -2, -1))
+        check areClose(firstRefAABB.max.Vec3f, newVec3f(5, 4, 2))
