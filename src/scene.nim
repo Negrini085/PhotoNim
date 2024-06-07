3ac05834426b3b58008fbb45227a261f3d926d14
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2024-06-07 11:37:21 +0200

    feat(Scene): new ReferenceSystem type. feat(SceneNode): better managing the building of the scene tree. feat(Scene): fromObserver proc returns a new Scene viewed from a given refSystem.

--- src/scene.nim
+++ src/scene.nim
@@ -1,29 +1,64 @@
-import geometry, hdrimage, camera, shapes
+import geometry, hdrimage, shapes
 
 from std/sequtils import concat, apply, map, foldl, toSeq
 from std/algorithm import sorted
 from std/math import copySign
 
 
-type ReferenceSystem* = ref object
-    origin*: Point3D
-    base*: ONB
+type ReferenceSystem* = tuple[origin: Point3D, base: Mat3f]
 
-proc newReferenceSystem*(origin = ORIGIN3D, base = stdONB): ReferenceSystem {.inline.} = 
-    ReferenceSystem(origin: origin, base: base)
+proc newReferenceSystem*(origin: Point3D, base = Mat3f.id): ReferenceSystem {.inline.} = (origin, base)
+
+proc newONB(normal: Normal): Mat3f = 
+    let
+        sign = copySign(1.0, normal.z)
+        a = -1.0 / (sign + normal.z)
+        b = a * normal.x * normal.y
+
+    [
+        newVec3f(1.0 + sign * a * normal.x * normal.x, sign * b, -sign * normal.x),
+        newVec3f(b, sign + a * normal.y * normal.y, -normal.y), 
+        normal.Vec3f
+    ]
+
+proc newReferenceSystem*(origin: Point3D, normal: Normal): ReferenceSystem {.inline.} = (origin, newONB(normal)) 
+
+proc coeff*(refSystem: ReferenceSystem, pt: Vec3f): Vec3f {.inline.} = dot(refSystem.base, pt)
+proc fromCoeff*(refSystem: ReferenceSystem, coeff: Vec3f): Vec3f {.inline.} = dot(refSystem.base.T, coeff)
 
 
-type 
-    ShapeHandler* = tuple[shape: Shape, transformation: Transformation]
+type ShapeHandler* = tuple[shape: Shape, transformation: Transformation]
     
-    SceneNodeKind* = enum
-        nkRoot, nkBranch, nkLeaf
+proc getAABB*(handler: ShapeHandler): Interval[Point3D] {.inline.} =
+    # This assumes the handler is in the local reference system
+    getAABB(handler.shape.getVertices.map(proc(v: Point3D): Point3D = apply(handler.transformation, v)))
+
+proc getAABB*(refSystem: ReferenceSystem, handler: ShapeHandler): Interval[Point3D] {.inline.} =
+    # This assumes the handler is in the global reference system and the function must return the aabb from the handler in the local reference system,
+    # so we need to use the complete transformation, not just the translation!
+    let transformation = newComposition(newTranslation(refSystem.origin).inverse, handler.transformation) 
+    getAABB(handler.shape.getVertices.map(proc(v: Point3D): Point3D = refSystem.coeff(apply(transformation, v).Vec3f).Point3D))
+
+proc getTotalAABB*(shapeHandlers: seq[ShapeHandler]): Interval[Point3D] =
+    # This assumes the handlers are in the local reference system
+    if shapeHandlers.len == 0: return (ORIGIN3D, ORIGIN3D)
+
+    result = (min: newPoint3D(Inf, Inf, Inf), max: newPoint3D(-Inf, -Inf, -Inf))
+
+    var aabb: Interval[Point3D]
+    for i in 0..<shapeHandlers.len:
+        aabb = shapeHandlers[i].getAABB
+        result = newInterval(newInterval(aabb.min, result.min).min, newInterval(aabb.max, result.max).max)
+
+
+type
+    SceneNodeKind* = enum nkBranch, nkLeaf
 
     SceneNode* = ref object
         aabb*: Interval[Point3D]
         
         case kind*: SceneNodeKind
-        of nkRoot, nkBranch:
+        of nkBranch:
             left*, right*: SceneNode
         of nkLeaf:
             handlers*: seq[ShapeHandler]
@@ -31,67 +66,55 @@ type
     Scene* = object
         bgCol*: Color
         handlers*: seq[ShapeHandler]
+        rs*: ReferenceSystem = newReferenceSystem(ORIGIN3D) # maybe this is not useful to store
+        tree*: SceneNode = nil
 
 
-proc getWorldAABB*(handler: ShapeHandler): Interval[Point3D] {.inline.} =
-    getAABB(handler.shape.getVertices.map(proc(vertex: Point3D): Point3D = apply(handler.transformation, vertex)))
+proc newScene*(shapeHandlers: seq[ShapeHandler], bgCol: Color = BLACK): Scene {.inline.} = Scene(bgCol: bgCol, handlers: shapeHandlers)
+
+proc fromObserver*(scene: Scene; refSystem: ReferenceSystem): Scene =
+    let ## here we need to use the complete transformation, not just the translation!
+        localTransformation = newTranslation(refSystem.origin)
+        localShapeHandlers = scene.handlers.map(proc(handler: ShapeHandler): ShapeHandler = 
+            (shape: handler.shape, transformation: newComposition(localTransformation.inverse, handler.transformation))
+        ) # these must be local shapeHandlers! 
+
+    Scene(bgCol: scene.bgCol, handlers: localShapeHandlers, rs: refSystem)
 
 
-proc getAABB*(refSystem: ReferenceSystem; handler: ShapeHandler): Interval[Point3D] {.inline.} =
-    let localTransformation = newComposition(newTranslation(refSystem.origin.Vec3f).inverse, handler.transformation) 
-    getAABB(
-        handler.shape.getVertices
-            .map(proc(vertex: Point3D): Point3D = 
-                refSystem.base.getComponents(
-                    apply(localTransformation, vertex).Vec3f
-                ).Point3D
-            )
-    )         
+type BVHStrategy* = enum skMedian, skSAH
 
-proc getAABB*(refSystem: ReferenceSystem; shapeHandlers: seq[ShapeHandler]): Interval[Point3D] =
-    if shapeHandlers.len == 0: return (refSystem.origin, refSystem.origin)
-
-    result = (min: newPoint3D(Inf, Inf, Inf), max: newPoint3D(-Inf, -Inf, -Inf))
-    for i in 0..<shapeHandlers.len:
-        let aabb = refSystem.getAABB(shapeHandlers[i])
-        result = newInterval(newInterval(aabb.min, result.min).min, newInterval(aabb.max, result.max).max)
-
-
-proc newBVHNode(refSystem: ReferenceSystem, shapeHandlers: seq[ShapeHandler], maxShapesPerLeaf, depth: int): SceneNode =   
+proc newBVHNode(shapeHandlers: seq[ShapeHandler], maxShapesPerLeaf, depth: int, strategy: BVHStrategy): SceneNode =   
     if shapeHandlers.len == 0: return nil
 
-    let aabb = refSystem.getAABB(shapeHandlers)
+    let aabb = shapeHandlers.getTotalAABB
 
-    if shapeHandlers.len <= maxShapesPerLeaf: 
-        return SceneNode(kind: nkLeaf, aabb: aabb, handlers: shapeHandlers)
+    if shapeHandlers.len <= maxShapesPerLeaf: return SceneNode(kind: nkLeaf, aabb: aabb, handlers: shapeHandlers)
 
     let 
-        sortedShapes = shapeHandlers.sorted(proc(a, b: ShapeHandler): int = cmp(refSystem.getAABB(a).min.x, refSystem.getAABB(b).min.x))
-        leftNode =  newBVHNode(refSystem, sortedShapes[0..<sortedShapes.len div 2], maxShapesPerLeaf, depth + 1)
-        rightNode = newBVHNode(refSystem, sortedShapes[sortedShapes.len div 2..<sortedShapes.len], maxShapesPerLeaf, depth + 1)
+        sortedShapes = shapeHandlers.sorted(
+            proc(a, b: ShapeHandler): int =
+                cmp(a.getAABB.min.Vec3f[depth mod 3], b.getAABB.min.Vec3f[depth mod 3]) # here something is wrong
+        )
+        leftNode = newBVHNode(sortedShapes[0..<sortedShapes.len div 2], maxShapesPerLeaf, depth + 1, strategy)
+        rightNode = newBVHNode(sortedShapes[sortedShapes.len div 2..<sortedShapes.len], maxShapesPerLeaf, depth + 1, strategy)
     
-    SceneNode(
-        kind: nkBranch,
-        aabb: aabb, 
-        left: leftNode, 
-        right: rightNode
-    )
+    SceneNode(kind: nkBranch, aabb: aabb, left: leftNode, right: rightNode)
 
-
-proc buildSceneTree*(refSystem: ReferenceSystem; scene: Scene, maxShapesPerLeaf: int): SceneNode =  
-    result = newBVHNode(refSystem, scene.handlers.map(), maxShapesPerLeaf, depth = 0)
-    result.kind = nkRoot
-
-proc newScene*(shapeHandlers: seq[ShapeHandler], bgCol: Color = BLACK): Scene {.inline.} = 
-    Scene(bgCol: bgCol, handlers: shapeHandlers)
+proc buildBVHTree*(scene: var Scene; maxShapesPerLeaf: int, strategy: BVHStrategy) {.inline.} = 
+    # Scene.handlers is always referred from the local reference system
+    scene.tree = newBVHNode(scene.handlers, maxShapesPerLeaf, depth = 0, strategy)
 
 
 proc loadMesh*(world: Scene, source: string) = quit "to implement"
 proc loadTexture*(world: Scene, source: string, shape: Shape) = quit "to implement"
 
 
+proc newShapeHandler*(shape: Shape, transformation = Transformation.id): ShapeHandler {.inline.} =
+    (shape: shape, transformation: transformation)
+
 proc newSphere*(center: Point3D, radius: float32; material = newMaterial()): ShapeHandler {.inline.} =   
-    (shape: newSphere(radius, material), transformation: if center != ORIGIN3D: newTranslation(center.Vec3f) else: IDENTITY)
+    newShapeHandler(newSphere(radius, material), if center != ORIGIN3D: newTranslation(center) else: Transformation.id)
 
 proc newUnitarySphere*(center: Point3D; material = newMaterial()): ShapeHandler {.inline.} = 
-    (shape: newSphere(1.0, material), transformation: if center != ORIGIN3D: newTranslation(center.Vec3f) else: IDENTITY)
+    newShapeHandler(newSphere(1.0, material), if center != ORIGIN3D: newTranslation(center) else: Transformation.id)
--- tests/scene.nim
+++ tests/scene.nim
@@ -2,36 +2,42 @@ import std/[unittest, sequtils]
 import PhotoNim
 
 suite "Scene unittest":
-    test "getWorldAABB proc":
-
+    setup:
         let 
-            handler = (newTriangle(newPoint3D(0, -2, 0), newPoint3D(2, 1, 1), newPoint3D(0, 3, 0)), IDENTITY)
-            worldRefSystem = newReferenceSystem(ORIGIN3D, stdONB)
-            otherRefSystem = newReferenceSystem(newPoint3D(5.0, 0.0, 0.0), newONB(-eX, eY, -eZ))
-            oRefSystem = newReferenceSystem(newPoint3D(5.0, 0.0, 0.0), stdONB)
+            triangle = newTriangle(newPoint3D(0, -2, 0), newPoint3D(2, 1, 1), newPoint3D(0, 3, 0))
+            scene = newScene(@[newShapeHandler(triangle), newShapeHandler(triangle, newTranslation([float32 1, 1, -2]))])
+
+    test "newScene proc":
+        check scene.bgCol == BLACK
+
+        check scene.handlers.len == 2
+        check scene.handlers[0].shape.kind == skTriangle
+        check scene.handlers[1].shape.kind == skTriangle
+
+        check scene.tree.isNil
+
+    test "getTotalAABB proc":
+        let aabb = scene.handlers.getTotalAABB
+
+        check aabb.min == newPoint3D(0, -2, -2)
+        check aabb.max == newPoint3D(3, 4, 1)
+        
+    test "fromObserver proc":
+        var subScene = scene.fromObserver(newReferenceSystem(newPoint3D(5.0, 0.0, 0.0), [-eX, eY, -eZ]))
+
+        check subScene.bgCol == BLACK
+
+        check subScene.handlers.len == 2
+        check scene.handlers[0].shape.kind == skTriangle
+        check scene.handlers[1].shape.kind == skTriangle
+
     
-            worldAABB = handler.getWorldAABB
-            otherAABB = otherRefSystem.getAABB(handler)
-            pAABB = oRefSystem.getAABB(handler)
+    test "buildBVHTree proc":
+        var subScene = scene.fromObserver(newReferenceSystem(newPoint3D(5.0, 0.0, 0.0), [-eX, eY, -eZ]))
 
+        check subScene.tree.isNil
+        subScene.buildBVHTree(maxShapesPerLeaf = 4, skSAH)
+        check not subScene.tree.isNil
 
-        check areClose(worldAABB.min, newPoint3D(0, -2, 0))
-        check areClose(worldAABB.max, newPoint3D(2, 3, 1))
-
-        check areClose(otherAABB.min, newPoint3D(3, -2, -1))
-        check areClose(otherAABB.max, newPoint3D(5, 3, 0))
-
-        check areClose(pAABB.min, newPoint3D(-5, -2, 0))
-        check areClose(pAABB.max, newPoint3D(-3, 3, 1))
-        
-
-    test "getAABB proc":
-        let 
-            handler1 = (newTriangle(newPoint3D(0, -2, 0), newPoint3D(2, 1, 1), newPoint3D(0, 3, 0)), newTranslation([float32 1, 1, -2]))
-            handler2 = (newTriangle(newPoint3D(0, -2, 0), newPoint3D(2, 1, 1), newPoint3D(0, 3, 0)), IDENTITY)
-            worldRefSystem = newReferenceSystem(ORIGIN3D, stdONB)
-            otherRefSystem = newReferenceSystem(newPoint3D(5.0, 0.0, 0.0), newONB(-eX, eY, -eZ))
-        
-            sceneAABB = otherRefSystem.getAABB(@[handler1, handler2])
-
-        echo sceneAABB
+        check subScene.tree.aabb.min == newPoint3D(2, -2, -1)
+        check subScene.tree.aabb.max == newPoint3D(5, 4, 2)
