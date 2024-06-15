db301f2e62e376508c1453b288f2b7ed71f3dbd3
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-06-15 12:06:09 +0200

    refactor(ReferenceSystem): change project & compose procs, now accounting for frame of reference translation

--- src/geometry.nim
+++ src/geometry.nim
@@ -675,8 +675,21 @@ proc newReferenceSystem*(origin: Point3D, normal: Normal): ReferenceSystem {.inl
     newReferenceSystem(origin, newRightHandedBase(newONB(normal))) 
 
 
-proc project*[V](refSystem: ReferenceSystem, pt: V): Vec3f {.inline.} = dot(refSystem.base, when V is Vec3f: pt else: pt.Vec3f)
-proc compose*[V](refSystem: ReferenceSystem, coeff: V): Vec3f {.inline.} = dot(refSystem.base.T, when V is Vec3f: coeff else: coeff.Vec3f)
+proc project*[V](refSystem: ReferenceSystem, obj: V): V {.inline.} = 
+    when V is Point3D:
+        dot(refSystem.base, (obj - refSystem.origin).Vec3f).Point3D
+    elif V is Vec3f:
+        dot(refSystem.base, obj)
+    else:
+        dot(refSystem.base, obj.Vec3f).V
+
+proc getWorldObject*[V](refSystem: ReferenceSystem, coeff: V): V {.inline.} = 
+    when V is Point3D:
+        dot(refSystem.base.T, coeff.Vec3f) + refSystem.origin
+    elif V is Vec3f:
+        dot(refSystem.base.T, coeff)
+    else:
+        dot(refSystem.base.T, coeff.Vec3f).V
 
 proc getTransformation*(refSystem: ReferenceSystem): Transformation {.inline.} =
     newComposition(newTranslation(refSystem.origin), newTransformation(refSystem.base.toMat4.T, refSystem.base.toMat4))
