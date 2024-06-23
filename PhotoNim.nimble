a47f0498c825601e3c0d561240d5346da14f027a
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2024-06-23 10:28:03 +0200

    refactor: demo and demoAnim nimble tasks

--- PhotoNim.nimble
+++ PhotoNim.nimble
@@ -1,5 +1,5 @@
 # Package
-version       = "0.1.0"
+version       = "0.3.0"
 author        = "lorenzoliuzzo & Negrini085"
 description   = "A CPU raytracer written in Nim"
 license       = "GPL-3.0-or-later"
@@ -19,34 +19,45 @@ task build, "Build the `PhotoNim` executable\n":
 task demo, """Run the `PhotoNim` demo!
 
           Usage: 
-                  nimble demo (persp | ortho) (OnOff | Flat | Path) [--w=<width> --h=<height> --angle=<angle>] [<output>]
+                  nimble demo (persp | ortho) (OnOff | Flat | Path) <angle> [<output>] [<width> <height>]
 
           Options:
+
                   persp | ortho          Camera kind: Perspective or Orthogonal
+
                   OnOff | Flat | Path    Renderer kind: OnOff (only shows hit), Flat (flat renderer), Path (path tracer)
 
-                  --w=<width>            Image width. [default: 1600]
-                  --h=<height>           Image height. [default: 900]
-                  --angle=<angle>        Rotation angle around z axis. [default: 10]
+                  <angle>                Rotation angle around z axis. [default: 10]
 
-                  <output>               Path to the output HDRImage. [default: "examples/demo/demo.pfm"]
+                  <output>               Path to the LDRImage output. [default: "examples/demo/demo.png"]
+                  
+                  <width>                Image width. [default: 900]
+                  <height>               Image height. [default: 900]
+
 """:
 
     var 
-        demoCommand = "nim c -d:release -r examples/demo/main.nim"
+        demoCommand = "nim c -d:release --hints:off -r examples/demo/main.nim"
         commands = newSeq[string](paramCount() + 1)
 
     for i in 0..paramCount(): commands[i] = paramStr(i)
     for i in commands.find("demo")..paramCount(): demoCommand.add(" " & paramStr(i))
 
     exec demoCommand
-    exec "rm examples/demo/main"
+    
 
-
-task demoAnim, "Run the PhotoNim demo animation":
-  exec "nim c -d:release PhotoNim.nim"
+task demoAnim, "Run the `PhotoNim` demo animation":
   exec "sh examples/demo/animation.sh"
-  
+  exec "open examples/demo/demo.mp4"
+
+
+# task earth, "Run the Earth animation!":
+#   exec "nim c -d:release --hints:off examples/earth/main.nim"
+#   exec "seq 0 359 | parallel -j 8 --eta './examples/earth/main {1}'"
+#   exec "ffmpeg -framerate 30 -i examples/earth/frames/img%03d.png -c:v libx264 -pix_fmt yuv420p examples/earth/earth.mp4 -y"
+#   exec "open examples/earth/earth.mp4"
+
+
 task examples, "Run the PhotoNim examples":
   exec "nim c -d:release --hints:off -r examples/shapes/triangle.nim"
   exec "nim c -d:release --hints:off -r examples/shapes/box.nim"
--- examples/demo/main.nim
+++ examples/demo/main.nim
@@ -4,8 +4,6 @@ import docopt
 
 from std/times import cpuTime
 from std/strformat import fmt
-from std/os import splitFile
-from std/osproc import execCmd
 from std/cmdline import commandLineParams
 from std/strutils import parseInt, parseFloat
 
@@ -14,17 +12,19 @@ let demoDoc* = """
 Welcome to `PhotoNim`, run this demo example and start rendering!
 
 Usage: 
-        nimble demo (persp | ortho) (OnOff | Flat | Path) [--w=<width> --h=<height> --angle=<angle>] [<output>]
+        nimble demo (persp | ortho) (OnOff | Flat | Path) [<angle>] [<output>] [<width> <height>]
 
 Options:
         persp | ortho          Camera kind: Perspective or Orthogonal
+
         OnOff | Flat | Path    Renderer kind: OnOff (only shows hit), Flat (flat renderer), Path (path tracer)
 
-        --w=<width>            Image width. [default: 1600]
-        --h=<height>           Image height. [default: 900]
-        --angle=<angle>        Rotation angle around z axis. [default: 10]
+        <angle>                Rotation angle around z axis. [default: 10.0]
 
-        <output>               Path to the output HDRImage. [default: "examples/demo/demo.pfm"]
+        <output>               Path to the LDRImage output. [default: "examples/demo/demo.png"]
+
+        <width>                Image width. [default: 900]
+        <height>               Image height. [default: 900]
 """
 
 proc demoScene*(): Scene =
@@ -72,46 +72,29 @@ proc demoScene*(): Scene =
     newScene(@[s0, s1, s2, s3, s4, s5, s6, s7, s8, s9])
 
 
-let args = docopt(demoDoc, argv=commandLineParams())
+let 
+    args = docopt(demoDoc, argv=commandLineParams())
 
-var 
-    (width, height) = (1600, 900)
-    angle = 10.0
+    width = if args["<width>"]: parseInt($args["<width>"]) else: 900
+    height = if args["<height>"]: parseInt($args["<height>"]) else: 900
 
-if args["--w"]: 
-    try: width = parseInt($args["--w"]) 
-    except: echo "Warning: width must be an integer. Default value is used."
-
-if args["--h"]: 
-    try: height = parseInt($args["--h"]) 
-    except: echo "Warning: height must be an integer. Default value is used."
-
-if args["--angle"]:
-    try: angle = parseFloat($args["--angle"]) 
-    except: echo "Warning: angle must be an integer. Default value is used."
-
- 
-let
-    pfmOut = if args["<output>"]: $args["<output>"] else: "examples/demo/demo.pfm"
-    (dir, name, _) = splitFile(pfmOut)
-    pngOut = dir & '/' & name & ".png"
-    
     renderer = 
         if args["OnOff"]: newOnOffRenderer(hitCol = newColor(1, 215.0 / 255, 0))
         elif args["Flat"]: newFlatRenderer()
-        else: newPathTracer(numRays = 9, maxDepth = 1, rouletteLimit = 3)
+        else: newPathTracer(numRays = 5, maxDepth = 5, rouletteLimit = 3)
+
+    angle = if args["<angle>"]: parseFloat($args["<angle>"]) else: 10    
+
+    pngOut = if args["<output>"]: $args["<output>"] else: "examples/demo/demo.png"
 
     camera = 
-        if args["persp"]: newPerspectiveCamera(renderer, (width, height), 3.0, newComposition(newRotZ(angle), newTranslation(-eX))) 
-        else: newOrthogonalCamera(renderer, (width, height))
-    
+        if args["persp"]: newPerspectiveCamera(renderer, (width, height), 1.0, newComposition(newRotZ(angle), newTranslation(-eX))) 
+        else: newOrthogonalCamera(renderer, (width, height), newRotZ(angle))
+        
     timeStart = cpuTime()
+    image = camera.sample(demoScene(), rgState = 42, rgSeq = 4, samplesPerSide = 2, maxShapesPerLeaf = 3)
 
-    image = camera.sample(demoScene(), rgState = 42, rgSeq = 4, samplesPerSide = 1, maxShapesPerLeaf = 2)
+echo fmt"Successfully rendered image in {cpuTime() - timeStart:.6f} seconds."
 
-echo fmt"Successfully rendered image in {cpuTime() - timeStart} seconds."
-
-image.savePFM(pfmOut)
 image.savePNG(pngOut, 0.18, 1.0, 0.1)
-
-discard execCmd fmt"open {pngOut}"
+echo fmt"Successfully saved image to {pngOut}."
