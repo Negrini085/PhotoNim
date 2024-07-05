b0efd945cafc49482a127d53dc1f6825ce7f0013
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2024-07-05 17:46:25 +0200

    feat(CookTorranceBRDF): corrected sampling pdf.

--- src/material.nim
+++ src/material.nim
@@ -1,9 +1,7 @@
-import std/streams
 import geometry, pcg, hdrimage
 
-from std/math import floor, pow, sqrt, exp, ln, sin, cos, arccos, PI, degToRad
-from std/streams import newFileStream, close
-from std/strformat import fmt
+from std/math import floor, pow, sqrt, exp, ln, sin, cos, arccos, arctan, PI, degToRad
+from std/streams import FileStream, newFileStream, close
 
 type
     PigmentKind* = enum pkUniform, pkTexture, pkCheckered
@@ -17,7 +15,7 @@ type
         of pkCheckered:
             grid*: tuple[c1, c2: Color, nRows, nCols: int]
 
-    CookTorranceNDF* = enum ndfGGX, ndfBeckmann, ndfBlinn
+    CookTorranceNDF* = enum ndfGGX, ndfBeckmann
 
     BRDFKind* = enum LambertianBRDF, FresnelMetalBRDF, CookTorranceBRDF
 
@@ -35,8 +33,8 @@ type
             diffuseCoeff*: float32
             specularCoeff*: float32
             roughness*: float32
-            albedo*: float32
-            metallic*: float32
+            refractionIndex*: float32
+
         
     Material* = tuple[brdf: BRDF, radiance: Pigment]
 
@@ -86,58 +84,51 @@ proc newFresnelMetalBRDF*(pigment = newUniformPigment(WHITE)): BRDF {.inline.} =
     BRDF(kind: FresnelMetalBRDF, pigment: pigment) 
 
 
-proc newCookTorranceBRDF*(pigment = newUniformPigment(WHITE), diffuseCoeff: float32 = 0.3, specularCoeff: float32 = 0.7, roughness: float32 = 0.5, albedo: float32 = 0.5, metallic: float32 = 0.1, ndf: CookTorranceNDF = CookTorranceNDF.ndfGGX): BRDF =
+proc newCookTorranceBRDF*(pigment = newUniformPigment(WHITE), diffuseCoeff: float32 = 0.3, specularCoeff: float32 = 0.7, roughness: float32 = 0.5, refractionIndex: float32 = 1.0, ndf: CookTorranceNDF = CookTorranceNDF.ndfGGX): BRDF =
     assert (diffuseCoeff + specularCoeff <= 1.0)
     BRDF(
         kind: CookTorranceBRDF, ndf: ndf, pigment: pigment, 
         diffuseCoeff: diffuseCoeff, 
         specularCoeff: specularCoeff, 
-        roughness: roughness, albedo: albedo, metallic: metallic
+        roughness: roughness, refractionIndex: refractionIndex 
     )
 
 
-proc distBeckmann(normal, inDir, outDir: Vec3f, roughness: float32): float32 =
-    let 
-        cos2 = pow(dot(normal, inDir + outDir), 2)
-        roughness2 = pow(roughness, 2)
-    
+proc BeckmannDistribution(normal, midDir: Vec3f, roughness: float32): float32 =
+    let (roughness2, cos2) = (pow(roughness, 2), pow(dot(normal, midDir), 2))
     exp((cos2 - 1) / (roughness2 * cos2)) / (PI * roughness2 * pow(cos2, 2))
-
-proc FresnelSchlick(normal, inDir, outDir: Vec3f, refractionIndex: float32): float32 = 
-    let F0 = pow(refractionIndex - 1, 2.0) / pow(refractionIndex + 1, 2.0)
-    return F0 + (1 - F0) * pow(1.0 - dot(normal, inDir + outDir), 5.0)
-
-proc geometricAttenuation(normal, inDir, outDir: Vec3f): float32 =
-    let 
-        halfVector = inDir + outDir
-        dot1 = dot(normal, halfVector)
-        dot2 = min(dot(normal, inDir), dot(normal, outDir))
     
-    min(1, 2 * dot1 * dot2 / dot(inDir, halfVector))
+
+proc GGXDistribution(normal, midDir: Vec3f, roughness: float32): float32 =
+    let roughness2 = pow(roughness, 2)
+    return roughness2 / (PI * pow(pow(dot(normal, midDir), 2) * (roughness2 - 1) + 1, 2))
+    
+
+proc FresnelSchlick(normal, midDir: Vec3f, refractionIndex: float32): float32 = 
+    let F0 = pow((refractionIndex - 1) / (refractionIndex + 1), 2)
+    return F0 + (1 - F0) * pow(1.0 - dot(normal, midDir), 5.0)
+
+proc GeometricAttenuation(normal, inDir, outDir, midDir: Vec3f): float32 {.inline.} =
+    min(1, 2 * dot(normal, midDir) * min(dot(normal, inDir), dot(normal, outDir)) / dot(inDir, midDir))
 
 
 proc eval*(brdf: BRDF; surfacePoint: Point2D, normal, inDir, outDir: Vec3f): float32 {.inline.} =
     case brdf.kind: 
-    of LambertianBRDF: return brdf.reflectance
+    of LambertianBRDF: return brdf.reflectance / PI
 
     of FresnelMetalBRDF: return 1.0
-        # assert areClose(normal.norm, 1.0, 1e-2), fmt"{normal.norm}"
-        # assert areClose(inDir.norm, 1.0, 1e-2), fmt"{inDir.norm}"
-        # assert areClose(outDir.norm, 1.0, 1e-2), fmt"{outDir.norm}"
-        # let (angleIn, angleOut) = (PI - arccos(dot(normal, inDir)), arccos(dot(normal, outDir)))
-        # # echo (angleIn, angleOut, angleIn - angleOut, angleIn + angleOut)
-        # # assert areClose(angleIn + angleOut, PI, 0.1)
-        # return if angleIn + angleOut <= brdf.thresholdAngle: 1.0 else: 0.0
-        # return if abs(arccos(dot(inDir, outDir))) <= brdf.thresholdAngle: 1.0 else: 0.0
 
     of CookTorranceBRDF: 
         let
-            D = distBeckmann(normal, inDir, outDir, brdf.roughness)
-            F = FresnelSchlick(normal, inDir, outDir, 1.5)
-            G = geometricAttenuation(normal, inDir, outDir)
-            kSpec = 0.25 * D * F * G / (dot(normal, inDir) * dot(normal, outDir))
+            midDir = inDir + outDir
+            D = case brdf.ndf
+                of ndfBeckmann: BeckmannDistribution(normal, midDir, brdf.roughness)
+                of ndfGGX: GGXDistribution(normal, midDir, brdf.roughness)
 
-        return brdf.diffuseCoeff / PI + brdf.specularCoeff * kSpec
+            F = FresnelSchlick(normal, midDir, brdf.refractionIndex)
+            G = GeometricAttenuation(normal, inDir, outDir, midDir)
+
+        return brdf.diffuseCoeff / PI + 0.25 * brdf.specularCoeff * D * F * G / (dot(normal, inDir) * dot(normal, outDir))
 
 
 proc scatterDir*(brdf: BRDF, hitNormal: Normal, hitDir: Vec3f, rg: var PCG): Vec3f =
@@ -145,10 +136,10 @@ proc scatterDir*(brdf: BRDF, hitNormal: Normal, hitDir: Vec3f, rg: var PCG): Vec
     of LambertianBRDF:
         let 
             (cos2, phi) = (rg.rand, 2 * PI.float32 * rg.rand)
-            c = sqrt(cos2)
+            cos = sqrt(cos2)
             base = newONB(hitNormal)
         
-        return c * cos(phi) * base[0] + c * sin(phi) * base[1] + sqrt(1 - cos2) * base[2]
+        return cos * cos(phi) * base[0] + cos * sin(phi) * base[1] + sqrt(1 - cos2) * base[2]
 
     of FresnelMetalBRDF: 
         return hitDir - 2 * dot(hitNormal.Vec3f, hitDir) * hitNormal.Vec3f
@@ -156,15 +147,13 @@ proc scatterDir*(brdf: BRDF, hitNormal: Normal, hitDir: Vec3f, rg: var PCG): Vec
     of CookTorranceBRDF:
         let
             base = newONB(hitNormal)
-            rough2 = pow(brdf.roughness, 2.0)
+            rand = rg.rand
 
+            phi = 2 * PI * rg.rand
             theta = 
                 case brdf.ndf
-                of ndfGGX: arccos(sqrt(rough2 / (rg.rand * (rough2 - 1) + 1)))
-                of ndfBeckmann: arccos(sqrt(1 / (1 - rough2 * ln(1 - rg.rand))))
-                of ndfBlinn: arccos(1.0 / pow(rg.rand, brdf.roughness + 1))
-
-            phi = rg.rand
+                of ndfBeckmann: arctan(sqrt(-pow(brdf.roughness, 2.0) * ln(1 - rand)))
+                of ndfGGX: arctan(brdf.roughness * sqrt(rand) / sqrt(1 - rand))
 
         return base[0] * sin(theta) * cos(phi) + base[1] * sin(theta) * sin(phi) + base[2] * cos(theta)
 
