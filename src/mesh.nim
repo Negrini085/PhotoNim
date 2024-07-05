59a0e113cfb01d3e61fbcbcdbfe37a376ed10ccb
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2024-07-05 20:27:49 +0200

    refactor: shape.nim contains now the procedure to create an ObjectHandler of kind hkShape, similar with mesh.nim (to implement better in future)

--- src/mesh.nim
+++ src/mesh.nim
@@ -18,3 +18,34 @@
 #             tree: newBVHNode(triangles, depth = 0, treeKind.int, maxShapesPerLeaf, rg), 
 #         ), transformation
 #     )
+
+# proc loadMesh*(source: string): tuple[nodes: seq[Point3D], edges: seq[int]] = 
+#     var istream = newFileStream(source, fmRead)
+
+#     if istream.isNil: quit fmt"Error! Cannot open file {source}."
+
+#     while not istream.atEnd:
+
+#         var line = 
+#             try: istream.readLine 
+#             except: quit fmt"Error! Some failing happend while reading {source}."
+
+#         # I want to do further check only if we actually got something
+#         if not line.isEmptyOrWhitespace:
+#             let items = line.splitWhitespace.toSeq
+            
+#             if items[0] == "v": result.nodes.add newPoint3D(items[1].parseFloat, items[2].parseFloat, items[3].parseFloat)
+#             elif items[0] == "f": 
+#                 # We have only three indeces, so that we are defining only one triangular face
+#                 if items.len == 4: 
+#                     # We want to have infos only regarding faces (we are going to discard normals and additional stuff)
+#                     result.edges.add items[1].rsplit('/')[0].parseInt - 1 
+#                     result.edges.add items[2].rsplit('/')[0].parseInt - 1
+#                     result.edges.add items[3].rsplit('/')[0].parseInt - 1
+
+#                 else:
+#                     # Here we are triangulating non triangular meshes
+#                     for i in 0..items.len-4:
+#                         result.edges.add items[1].rsplit('/')[0].parseInt - 1 
+#                         result.edges.add items[2+i].rsplit('/')[0].parseInt - 1
+#                         result.edges.add items[3+i].rsplit('/')[0].parseInt - 1
