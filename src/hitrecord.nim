ff285883cd6bc12cc4c5f2842519cc0916aa8733
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-07-10 12:05:46 +0200

    feat(HitPayload): add getAllHitPayload proc

--- src/hitrecord.nim
+++ src/hitrecord.nim
@@ -52,7 +52,164 @@ type HitPayload* = object
     handler*: ShapeHandler
     ray*: Ray
     t*: float32
-    
+
+
+proc getAllHitPayload*(handler: ShapeHandler, worldInvRay: Ray): Option[seq[HitPayload]] =
+    # Procedure to get all HitPayload, necessary to have CSGDiff and CSGInt
+
+    case handler.shape.kind
+    of skAABox:
+        let
+            (min, max) = (handler.shape.aabb.min - worldInvRay.origin, handler.shape.aabb.max - worldInvRay.origin)
+            txSpan = newInterval(min.x / worldInvRay.dir[0], max.x / worldInvRay.dir[0])
+            tySpan = newInterval(min.y / worldInvRay.dir[1], max.y / worldInvRay.dir[1])
+
+        if txSpan.min > tySpan.max or tySpan.min > txSpan.max: return none seq[HitPayload]
+
+        let tzSpan = newInterval(min.z / worldInvRay.dir[2], max.z / worldInvRay.dir[2])
+        var hitSpan = newInterval(max(txSpan.min, tySpan.min), min(txSpan.max, tySpan.max))
+
+        if hitSpan.min > tzSpan.max or tzSpan.min > hitSpan.max: return none seq[HitPayload]
+
+        if tzSpan.min > hitSpan.min: hitSpan.min = tzSpan.min
+        if tzSpan.max < hitSpan.max: hitSpan.max = tzSpan.max
+
+        let tHit = if handler.shape.aabb.contains(worldInvRay.origin): hitSpan.max else: hitSpan.min
+        if not worldInvRay.tspan.contains(tHit): return none seq[HitPayload]
+
+        if tHit == hitSpan.max:
+            return some @[HitPayload(handler: handler, ray: worldinvRay, t: tHit)]
+        
+        return some @[
+            HitPayload(handler: handler, ray: worldinvRay, t: hitSpan.min),
+            HitPayload(handler: handler, ray: worldinvRay, t: hitSpan.max)
+            ]
+
+    of skTriangle:
+        let 
+            mat = [
+                [handler.shape.vertices[1].x - handler.shape.vertices[0].x, handler.shape.vertices[2].x - handler.shape.vertices[0].x, -worldInvRay.dir[0]], 
+                [handler.shape.vertices[1].y - handler.shape.vertices[0].y, handler.shape.vertices[2].y - handler.shape.vertices[0].y, -worldInvRay.dir[1]], 
+                [handler.shape.vertices[1].z - handler.shape.vertices[0].z, handler.shape.vertices[2].z - handler.shape.vertices[0].z, -worldInvRay.dir[2]]
+            ]
+            vec = [worldInvRay.origin.x - handler.shape.vertices[0].x, worldInvRay.origin.y - handler.shape.vertices[0].y, worldInvRay.origin.z - handler.shape.vertices[0].z]
+
+        let sol = try: solve(mat, vec) except ValueError: return none seq[HitPayload]
+        if not worldInvRay.tspan.contains(sol[2]): return none seq[HitPayload]
+        if sol[0] < 0.0 or sol[1] < 0.0 or sol[0] + sol[1] > 1.0: return none seq[HitPayload]
+
+        return some @[HitPayload(handler: handler, ray: worldInvRay, t: sol[2])]
+
+    of skSphere:
+        let (a, b, c) = (norm2(worldInvRay.dir), dot(worldInvRay.origin.Vec3f, worldInvRay.dir), norm2(worldInvRay.origin.Vec3f) - handler.shape.radius * handler.shape.radius)
+        let delta_4 = b * b - a * c
+        if delta_4 < 0: return none seq[HitPayload]
+
+        let (t_l, t_r) = ((-b - sqrt(delta_4)) / a, (-b + sqrt(delta_4)) / a)
+        let tHit = 
+            if worldInvRay.tspan.contains(t_l): t_l 
+            elif worldInvRay.tspan.contains(t_r): t_r 
+            else: return none seq[HitPayload]
+        
+        if tHit == t_r:
+            return some @[HitPayload(handler: handler, ray: worldInvRay, t: tHit)]
+        
+        return some @[
+            HitPayload(handler: handler, ray: worldInvRay, t: t_l),
+            HitPayload(handler: handler, ray: worldInvRay, t: t_r)
+            ]
+
+    of skPlane:
+        if abs(worldInvRay.dir[2]) < epsilon(float32): return none seq[HitPayload]
+        let tHit = -worldInvRay.origin.z / worldInvRay.dir[2]
+        if not worldInvRay.tspan.contains(t_hit): return none seq[HitPayload]
+
+        return some @[HitPayload(handler: handler, ray: worldInvRay, t: tHit)]
+
+    of skCylinder:
+        let
+            a = worldInvRay.dir[0] * worldInvRay.dir[0] + worldInvRay.dir[1] * worldInvRay.dir[1]
+            b = 2 * (worldInvRay.dir[0] * worldInvRay.origin.x + worldInvRay.dir[1] * worldInvRay.origin.y)
+            c = worldInvRay.origin.x * worldInvRay.origin.x + worldInvRay.origin.y * worldInvRay.origin.y - handler.shape.R * handler.shape.R
+            delta = b * b - 4.0 * a * c
+
+        if delta < 0.0: return none seq[HitPayload]
+
+        var tspan = newInterval((-b - sqrt(delta)) / (2 * a), (-b + sqrt(delta)) / (2 * a))
+
+        if tspan.min > worldInvRay.tspan.max or tspan.max < worldInvRay.tspan.min: return none seq[HitPayload]
+
+        var tHit = tspan.min
+        if tHit < worldInvRay.tspan.min:
+            if tspan.max > worldInvRay.tspan.max: return none seq[HitPayload]
+            tHit = tspan.max
+
+        var hitPt = worldInvRay.at(tHit)
+        var phi = arctan2(hitPt.y, hitPt.x)
+        if phi < 0.0: phi += 2.0 * PI
+
+        if areClose(worldInvRay.dir[0], 0) and areClose(worldInvRay.dir[1], 0) and worldInvRay.dir[2] != 0:
+            if not areClose(pow(worldInvRay.origin.x, 2) + pow(worldInvRay.origin.y, 2), pow(handler.shape.R, 2)):
+                return none seq[HitPayload]
+
+        if hitPt.z < handler.shape.zSpan.min or hitPt.z > handler.shape.zSpan.max or phi > handler.shape.phiMax:
+            if tHit == tspan.max: return none seq[HitPayload]
+            tHit = tspan.max
+            if tHit > worldInvRay.tspan.max: return none seq[HitPayload]
+            
+            hitPt = worldInvRay.at(tHit)
+            phi = arctan2(hitPt.y, hitPt.x)
+            if phi < 0.0: phi += 2.0 * PI
+            if hitPt.z < handler.shape.zSpan.min or hitPt.z > handler.shape.zSpan.max or phi > handler.shape.phiMax: return none seq[HitPayload]
+
+        if tHit == tspan.max:
+            return some @[HitPayload(handler: handler, ray: worldInvRay, t: tHit)]
+        
+        return some @[
+            HitPayload(handler: handler, ray: worldInvRay, t: tspan.min),
+            HitPayload(handler: handler, ray: worldInvRay, t: tspan.max)
+            ]
+
+    of skEllipsoid: 
+        var 
+            hit: Option[seq[HitPayload]]
+            scal = newScaling(newVec3f(1/handler.shape.axis.a, 1/handler.shape.axis.b, 1/handler.shape.axis.c))
+        
+        hit = getAllHitPayload(
+            ShapeHandler(shape: Shape(kind: skSphere, radius: 1), transformation: handler.transformation),
+            worldInvRay.transform(scal)
+            )
+        
+        if hit.isNone: return none seq[HitPayload]
+        elif hit.get.len == 1:
+            return some @[HitPayload(handler: handler, ray: worldInvRay, t: hit.get[0].t)]
+        
+        return some @[
+            HitPayload(handler: handler, ray: worldInvRay, t: hit.get[0].t),
+            HitPayload(handler: handler, ray: worldInvRay, t: hit.get[1].t)
+            ]
+           
+    of skCSGUnion: 
+        var 
+            hitS: seq[Option[seq[HitPayload]]]
+            appo: seq[HitPayload]
+
+        hitS.add getAllHitPayload(
+            ShapeHandler(shape: handler.shape.shapes.primary, transformation: handler.shape.shTrans.tPrimary),
+            worldInvRay.transform(handler.shape.shTrans.tPrimary.inverse)
+            )
+        
+        hitS.add getAllHitPayload(
+            ShapeHandler(shape: handler.shape.shapes.secondary, transformation: handler.shape.shTrans.tSecondary),
+            worldInvRay.transform(handler.shape.shTrans.tSecondary.inverse)
+            )
+        
+        appo = hitS.filterIt(it.isSome).mapIt(it.get).concat
+        if appo.len == 0: return none seq[HitPayload]
+        return some appo
+        
+    of skTriangularMesh: discard
+
 
 proc getHitPayload*(handler: ShapeHandler, worldInvRay: Ray): Option[HitPayload] =
 
