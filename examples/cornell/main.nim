3a2fd9036c03bf8d4c6a53ec0c073d55017c96c0
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-07-12 12:38:36 +0200

    refactor(Cornell): correct Cornell example

--- examples/cornell/main.nim
+++ examples/cornell/main.nim
@@ -19,45 +19,44 @@ let
     filename = fmt"examples/cornell/frames/singlesample_{nRays}_{maxDepth}"
 
     renderer = newPathTracer(nRays, maxDepth)
-    camera = newPerspectiveCamera(renderer, viewport = (600, 600), distance = 1.0, newTranslation(newPoint3D(-0.75, 0, 0)))
+    camera = newPerspectiveCamera(renderer, viewport = (900, 900), distance = 1.0, newTranslation(newPoint3D(-0.75, 0, 0)))
 
 
     room = newBox(
         (newPoint3D(-2, -2, -2), newPoint3D(2, 2, 2)), 
-        newMaterial(newDiffuseBRDF(newUniformPigment(WHITE)))
+        newMaterial(newDiffuseBRDF(newUniformPigment(WHITE)), newUniformPigment(BLACK))
     )    
 
     lamp = newBox(
         (newPoint3D(0.5, -0.5, 1.8), newPoint3D(1.5, 0.5, 2)), 
-        newMaterial(newDiffuseBRDF(newUniformPigment(newColor(0.5, 0.5, 0.5))), newUniformPigment(WHITE))
+        newMaterial(newDiffuseBRDF(newUniformPigment(newColor(0.5, 0.5, 0.5))))
     ) 
 
     lwall = newBox(
         (newPoint3D(-2, 1.9, -2), newPoint3D(2, 2, 2)), 
-        newMaterial(newDiffuseBRDF(newUniformPigment(GREEN)))
+        newMaterial(newDiffuseBRDF(newUniformPigment(GREEN)), newUniformPigment(BLACK))
     ) 
     rwall = newBox(
         (newPoint3D(-2, -2, -2), newPoint3D(2, -1.9, 2)), 
-        newMaterial(newDiffuseBRDF(newUniformPigment(RED)))
+        newMaterial(newDiffuseBRDF(newUniformPigment(RED)), newUniformPigment(BLACK))
     ) 
 
     box1 = newBox(
         (newPoint3D(-0.5, -1.5, -2), newPoint3D(0.5, -0.9, 0.2)), 
-        newMaterial(newDiffuseBRDF(newUniformPigment(newColor(0.5, 0.5, 0.5)))), 
-        newRotZ(40)
+        newMaterial(newDiffuseBRDF(newUniformPigment(newColor(0.5, 0.5, 0.5))), newUniformPigment(BLACK)), 
+        newRotation(40, axisZ)
     )
 
     box2 = newBox(
         (newPoint3D(-0.5, 0.9, -2), newPoint3D(0.5, 1.5, 0.2)), 
-        newMaterial(newDiffuseBRDF(newUniformPigment(newColor(0.5, 0.5, 0.5)))), 
-        newRotZ(-40)
+        newMaterial(newDiffuseBRDF(newUniformPigment(newColor(0.5, 0.5, 0.5))), newUniformPigment(BLACK)), 
+        newRotation(-40, axisZ)
     )
 
 
     scene = newScene(@[room, lamp, lwall, rwall, box1, box2])
 
-
-    image = camera.sample(scene, rgState, rgSeq, samplesPerSide = 1, tkOctonary, maxShapesPerLeaf = 1)
+let image = camera.sample(scene, rgState, rgSeq, samplesPerSide = 1, tkOctonary, maxShapesPerLeaf = 1)
 
 
 echo fmt"Successfully rendered image in {cpuTime() - timeStart} seconds."
