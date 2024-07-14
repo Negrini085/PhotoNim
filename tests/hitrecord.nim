b0db78688b8f518a627f2598b245163adad8d721
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-07-14 14:29:31 +0200

    test(getLocalIntersection): add getLocalIntersection proc for available shape kinds

--- tests/hitrecord.nim
+++ tests/hitrecord.nim
@@ -2,6 +2,7 @@ import PhotoNim
 import std/[unittest, options]
 
 from std/sequtils import toSeq
+from std/math import PI
 
 
 #----------------------------------#
@@ -94,3 +95,160 @@ suite "HitPayload":
 
         # Checking HitPayload.rayDir
         check areClose(hitPayload.rayDir, eX)
+
+
+
+#--------------------------------------#
+#     LocalIntersection test suite     #
+#--------------------------------------#
+suite "LocalIntersection":
+    # LocalIntersection test suite, here we want to make sure that
+    # everything is good in intersection evaluation between a ray and a shape
+
+    setup:
+        var
+            t: float32
+
+    teardown:
+        discard t
+
+
+    test "Sphere":
+        # Checking getLocalIntersection for a ray-sphere intersection.
+        # Here we need to assure that time computation is indeed correct.
+        
+        let 
+            usphere = newUnitarySphere(
+                    ORIGIN3D, 
+                    newDiffuseBRDF(newUniformPigment(WHITE))
+                )
+            sphere = newSphere(
+                    newPoint3D(0, 1, 0), 3.0, 
+                    newDiffuseBRDF(newUniformPigment(WHITE))
+                )
+
+        var
+            ray1 = newRay(newPoint3D(0, 0, 2), -eZ)
+            ray2 = newRay(newPoint3D(3, 0, 0), -eX)
+            ray3 = newRay(ORIGIN3D, eX)
+
+        
+        # Unitary sphere
+        t = usphere.shape.getLocalIntersection(ray1.transform(usphere.transformation.inverse))
+        check areClose(t, 1)
+
+        t = usphere.shape.getLocalIntersection(ray2.transform(usphere.transformation.inverse))
+        check areClose(t, 2)
+
+        t = usphere.shape.getLocalIntersection(ray3.transform(usphere.transformation.inverse))
+        check areClose(t, 1)    
+
+        # Generic sphere
+        ray1.origin = newPoint3D(0, 1, 5)
+        ray2.origin = newPoint3D(4, 1, 0)
+        ray3.origin = newPoint3D(1, 1, 0)
+
+        t = sphere.shape.getLocalIntersection(ray1.transform(sphere.transformation.inverse))
+        check areClose(t, 2)
+
+        t = sphere.shape.getLocalIntersection(ray2.transform(sphere.transformation.inverse))
+        check areClose(t, 1)
+
+        t = sphere.shape.getLocalIntersection(ray3.transform(sphere.transformation.inverse))
+        check areClose(t, 2)    
+
+
+    test "Plane":
+        # Checking getLocalIntersection for a ray-plane intersection.
+        # Here we need to assure that time computation is indeed correct.
+
+        let plane = newPlane(newDiffuseBRDF(newUniformPigment(WHITE)))
+
+        var
+            ray1 = newRay(newPoint3D(0, 0, 2), -eZ)
+            ray2 = newRay(newPoint3D(1,-2,-3), newVec3f(0, 4/5, 3/5))
+            ray3 = newRay(newPoint3D(3, 0, 0), -eX)
+
+        
+        t = plane.shape.getLocalIntersection(ray1.transform(plane.transformation.inverse))
+        check areClose(t, 2)
+
+        t = plane.shape.getLocalIntersection(ray2.transform(plane.transformation.inverse))
+        check areClose(t, 5)
+
+        t = plane.shape.getLocalIntersection(ray3.transform(plane.transformation.inverse))
+        check t == Inf
+
+
+    test "Box":
+        # Checking getLocalIntersection for a ray-box intersection.
+        # Here we need to assure that time computation is indeed correct.
+
+        let box = newBox(
+            (newPoint3D(-1, 0, 1), newPoint3D(3, 2, 5)),
+            newDiffuseBRDF(newUniformPigment(BLACK))
+            )
+
+        var
+            ray1 = newRay(newPoint3D(-5, 1, 2), eX)
+            ray2 = newRay(newPoint3D(1, -2, 3), eY)
+            ray3 = newRay(newPoint3D(4, 1, 0), newVec3f(-1, 0, 0))
+
+          
+        t = box.shape.getLocalIntersection(ray1.transform(box.transformation.inverse))
+        check areClose(t, 4)
+
+        t = box.shape.getLocalIntersection(ray2.transform(box.transformation.inverse))
+        check areClose(t, 2)
+
+        t = box.shape.getLocalIntersection(ray3.transform(box.transformation.inverse)) 
+        check t == Inf
+
+
+    test "Triangle":
+        # Checking getLocalIntersection for a ray-triangle intersection.
+        # Here we need to assure that time computation is indeed correct.
+
+        let triangle = newTriangle(
+                [newPoint3D(3, 0, 0), newPoint3D(-2, 0, 0), newPoint3D(0.5, 2, 0)],
+                newDiffuseBRDF(newUniformPigment(WHITE))
+            )
+
+        var
+            ray1 = newRay(newPoint3D(0, 1, -2), eZ)
+            ray2 = newRay(newPoint3D(0, 1, -2), eX)
+
+        t = triangle.shape.getLocalIntersection(ray1.transform(triangle.transformation.inverse))
+        check areClose(t, 2)
+
+        t = triangle.shape.getLocalIntersection(ray2.transform(triangle.transformation.inverse))
+        check t == Inf
+
+
+    test "Cylinder":
+        # Checking getLocalIntersection for a ray-cylinder intersection.
+        # Here we need to assure that time computation is indeed correct.
+
+        let cylinder = newCylinder(
+                2, -2, 2, 2 * PI,
+                newDiffuseBRDF(newUniformPigment(WHITE))
+            )
+
+        var
+            ray1 = newRay(ORIGIN3D, eX)
+            ray2 = newRay(newPoint3D(4, 0, 0), -eX)
+            ray3 = newRay(newPoint3D(0, 0, -4), eZ)
+            ray4 = newRay(newPoint3D(2, 3, 1), eY)
+
+        t = cylinder.shape.getLocalIntersection(ray1.transform(cylinder.transformation.inverse))
+        check areClose(t, 2)
+
+        t = cylinder.shape.getLocalIntersection(ray2.transform(cylinder.transformation.inverse))
+        check areClose(t, 2)
+        
+        t = cylinder.shape.getLocalIntersection(ray3.transform(cylinder.transformation.inverse))
+        # check areClose(t, 2)                   
+
+        t = cylinder.shape.getLocalIntersection(ray4.transform(cylinder.transformation.inverse))
+        check t == Inf
+
