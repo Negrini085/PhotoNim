3d86d2482a7894871210abc837d2e31a82e67386
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2024-07-17 17:46:39 +0200

    second attempt

--- tests/renderer.nim
+++ tests/renderer.nim
@@ -66,9 +66,9 @@ suite "Rendering algorithms":
             mat = newMaterial(newDiffuseBRDF(newUniformPigment(WHITE)))
             sph = newSphere(ORIGIN3D, 0.2, mat)
 
-            scene = newScene(BLACK, @[sph], tkBinary, 1, (pcg.random, pcg.random))
-            camera = newPerspectiveCamera(newOnOffRenderer(), (3, 3), 2)
-            image = camera.sample(scene, (pcg.random, pcg.random))
+            scene = newScene(BLACK, @[sph], tkBinary, 1, newRandomSetUp(pcg))
+            camera = newPerspectiveCamera(newOnOffRenderer(WHITE), (3, 3), 2)
+            image = camera.sample(scene, newRandomSetUp(pcg))
 
         check areClose(image.getPixel(0, 0), BLACK)
         check areClose(image.getPixel(1, 0), BLACK)
@@ -88,7 +88,6 @@ suite "Rendering algorithms":
         # implemented is actually working or not 
 
         var pcg = newPCG(rs)
-
         let 
             mat = newEmissiveMaterial(
                 newDiffuseBRDF(newUniformPigment(newColor(0.2, 0.3, 0.5))),
@@ -96,10 +95,10 @@ suite "Rendering algorithms":
             )
 
             sph = newSphere(ORIGIN3D, 0.2, mat)
-            scene = newScene(BLACK, @[sph], tkBinary, 1, (pcg.random, pcg.random))
+            scene = newScene(BLACK, @[sph], tkBinary, 1, newRandomSetUp(pcg))
         
             camera = newPerspectiveCamera(newFlatRenderer(), (3, 3), 2)
-            image = camera.sample(scene, (pcg.random, pcg.random))
+            image = camera.sample(scene, newRandomSetUp(pcg))
 
         check areClose(image.getPixel(0, 0), BLACK)
         check areClose(image.getPixel(1, 0), BLACK)
